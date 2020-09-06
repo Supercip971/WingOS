ef17159bbb376d2d256d2f6aca5245c421bf13e2
Supercip971 <supercyp971@gmail.com>
2020-09-06 03:58:11 -0400

    better make cross compiler script now it is good

--- make_cross_compiler.sh
+++ make_cross_compiler.sh
@@ -3,7 +3,6 @@
 set -e
 # using : https://wiki.osdev.org/Cross-Compiler_Successful_Builds
 echo "creating wingOS_cross Compiler"
-echo "checking for dependencies"
 
 download_and_extract() {
         echo "Downloading $2"
@@ -19,48 +18,53 @@ binutils_file="binutils-$bin_utils_version.tar.xz"
 
 gcc_version="10.1.0"
 gcc_file="gcc-$gcc_version.tar.xz"
-        export PREFIX="$PWD/cross_compiler"
-        export TARGET="x86_64-pc-elf"
-        export PATH="$PREFIX/bin:$PATH"
-    mkdir -p ./cross_compiler/build/gcc ./cross_compiler/build/binutils
-    mkdir -p ./cross_compiler/src
-    cd ./cross_compiler/src
 
-    echo "downloading bin_utils"
-    download_and_extract "https://ftp.gnu.org/gnu/binutils/$binutils_file" "$binutils_file"
-    export binutils_src="$PWD/binutils-$bin_utils_version"
+export PREFIX="$PWD/cross_compiler"
+export TARGET="x86_64-pc-elf"
+export PATH="$PREFIX/bin:$PATH"
 
+mkdir -p ./cross_compiler/build/gcc ./cross_compiler/build/binutils
+mkdir -p ./cross_compiler/src
 
-    echo "downloading gcc"
-    download_and_extract "ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_version/$gcc_file" "$gcc_file"
-        export gcc_src="$PWD/gcc-$gcc_version"
+cd ./cross_compiler/src
+
+echo "downloading bin_utils"
+download_and_extract "https://ftp.gnu.org/gnu/binutils/$binutils_file" "$binutils_file"
+export binutils_src="$PWD/binutils-$bin_utils_version"
 
 
+echo "downloading gcc"
+download_and_extract "ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_version/$gcc_file" "$gcc_file"
+export gcc_src="$PWD/gcc-$gcc_version"
 
-    cd ..
-    cd build
 
-    echo "building bin_utils"
-    cd binutils
-        "$binutils_src/configure" --target="$TARGET" 	\
-                                                  --prefix="$PREFIX" 	\
-                                                      --with-sysroot 		\
-                                                  --disable-nls 		\
-                                                      --disable-werror
-           sudo make -j11
-        sudo make install -j11
-        cd ..
 
-    echo "building gcc"
-    cd gcc
-    "$gcc_src/configure" --target="$TARGET"			\
-                                                 --prefix="$PREFIX" 		\
-                                                 --disable-nls				\
-                                                 --enable-languages=c,c++	\
-                                                 --without-headers
+cd ..
+cd build
 
-sudo make all-gcc -j11
-sudo	make all-target-libgcc -j11
-sudo	make install-gcc -j11
-sudo	make install-target-libgcc -j11
-        cd ..
+echo "building bin_utils"
+cd binutils
+"$binutils_src/configure" --target="$TARGET" 	\
+        --prefix="$PREFIX" 	\
+        --with-sysroot 		\
+        --disable-nls 		\
+        --disable-werror
+
+make -j11
+make install -j11
+cd ..
+
+echo "building gcc"
+cd gcc
+"$gcc_src/configure" --target="$TARGET" \
+        --prefix="$PREFIX" 		\
+        --disable-nls			\
+        --enable-languages=c,c++	\
+        --with-newlib
+
+make all-gcc -j11
+make all-target-libgcc -j11
+make install-gcc -j11
+make install-target-libgcc -j11
+
+cd ..

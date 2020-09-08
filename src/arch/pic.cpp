e0c0c5ced9eb5360a1fea0bceee87077e50aad1e
Supercip971 <supercyp971@gmail.com>
2020-09-08 08:45:13 -0400

    little pic fix

--- src/arch/pic.cpp
+++ src/arch/pic.cpp
@@ -6,10 +6,6 @@
 
 void pic_init()
 {
-
-    uint8_t a1, a2;
-    a1 = inb(PIC1_DATA);
-    a2 = inb(PIC2_DATA);
     outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
     pic_wait();
     outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
@@ -25,13 +21,12 @@ void pic_init()
     outb(PIC2_DATA, 0x02);
     pic_wait();
 
-    outb(PIC1_DATA, 0x01);
+    outb(PIC1_DATA, 1);
     pic_wait();
-    outb(PIC2_DATA, 0x01);
+    outb(PIC2_DATA, 1);
     pic_wait();
 
-    outb(PIC1_DATA, a1);
-    outb(PIC2_DATA, a2);
-    outb(PIC1_DATA, 0xff); // unmask all for apic
+    outb(PIC1_DATA, 0xff); // mask all for apic
+    pic_wait();
     outb(PIC2_DATA, 0xff);
 }
--- src/arch/pic.h
+++ src/arch/pic.h
@@ -1,12 +1,12 @@
 #pragma once
 #define PIC1 0x20
 #define PIC1_COMMAND PIC1
-#define PIC1_OFFSET 0xa0
+#define PIC1_OFFSET 0x20
 #define PIC1_DATA (PIC1 + 1)
 
 #define PIC2 0xA0
 #define PIC2_COMMAND PIC2
-#define PIC2_OFFSET 0xa8
+#define PIC2_OFFSET 0x28
 #define PIC2_DATA (PIC2 + 1)
 
 #define ICW1_ICW4 0x01

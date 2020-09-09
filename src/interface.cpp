ad7c2418c081fa72f7d410127974b266dcb374c9
Supercip971 <supercyp971@gmail.com>
2020-09-09 13:03:31 -0400

    need to add _cxa_pure_virtual when using interface

---
+++ src/interface.cpp
@@ -0,0 +1,7 @@
+#include <interface.h>
+extern "C" void __cxa_pure_virtual()
+{
+
+    printf("error while trying to call a virtual function D: \n");
+    // Do nothing or print an error message.
+}
---
+++ src/interface.h
@@ -0,0 +1,4 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+#include <com.h>
+#endif // INTERFACE_H

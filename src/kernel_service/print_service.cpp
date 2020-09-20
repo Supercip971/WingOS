ea8e963d110d58d11091ff59a89a4c9353887540
Supercip971 <supercyp971@gmail.com>
2020-09-20 08:08:53 -0400

    print service : now use the given length

--- src/kernel_service/print_service.cpp
+++ src/kernel_service/print_service.cpp
@@ -1,4 +1,5 @@
 #include <arch/process.h>
+#include <com.h>
 #include <kernel_service/print_service.h>
 
 void print_service()
@@ -14,7 +15,8 @@ void print_service()
         else
         {
             asm("cli");
-            log("console_out", LOG_INFO) << (char *)msg->content_address; // this is very insecure :(
+            log("console_out", LOG_INFO); // this is very insecure :(
+            com_write_strn((char *)msg->content_address, msg->content_length);
             msg->has_been_readed = true;
             msg->entry_free_to_use = true;
             msg->response = 10; // 10 for yes

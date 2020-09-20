32df6dae9d04c664c43995560c14dae94c1260f0
Supercip971 <supercyp971@gmail.com>
2020-09-20 08:09:09 -0400

    usr lib : add stdarg.h

---
+++ usr_lib/stdarg.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+typedef __builtin_va_list va_list;
+#define va_start(ap, param) __builtin_va_start(ap, param)
+#define va_end(ap)          __builtin_va_end(ap)
+#define va_arg(ap, ap_type)    __builtin_va_arg(ap, ap_type)

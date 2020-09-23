fe69f301eadea84a07eab3aa7c935f633aa3c780
Supercip971 <supercyp971@gmail.com>
2020-09-23 16:29:45 -0400

    usr_lib: add strncmp

--- usr_lib/string.cpp
+++ usr_lib/string.cpp
@@ -14,6 +14,23 @@ int strcmp(const char* s1, const char* s2){
     }
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
+int strncmp(const char *s1, const char *s2, uint64_t n)
+{
+    while (n && *s1 && (*s1 == *s2))
+    {
+        ++s1;
+        ++s2;
+        --n;
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return (*(unsigned char *)s1 - *(unsigned char *)s2);
+    }
+}
 #ifndef WOS_OPTIMIZATION
 void* memcpy(void* dest, const void* src, uint64_t length){
     char* cdest = (char*) dest;
--- usr_lib/string.h
+++ usr_lib/string.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 uint64_t strlen(const char* s);
-
 int strcmp(const char* s1, const char* s2);
+int strncmp(const char *s1, const char *s2, uint64_t n);
 void* memcpy(void* dest, const void* src, uint64_t length);
+

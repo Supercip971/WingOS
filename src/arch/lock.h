377656271adc6fd09d4001b29534e7d96821763b
Supercip971 <supercyp971@gmail.com>
2020-09-23 12:43:23 -0400

    lock ! don't know if it really work

---
+++ src/arch/lock.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+struct lock_type
+{
+    uint32_t data;
+    // you can add more data
+};
+
+extern "C" void asm_spinlock_lock(volatile uint32_t *lock);
+extern "C" void asm_spinlock_unlock(volatile uint32_t *lock);
+
+// we put define so we can do
+
+#define lock(a) \
+    asm_spinlock_lock(&a->data);
+
+#define unlock(a) \
+    asm_spinlock_unlock(&a->data);
--- src/arch/utility.asm
+++ src/arch/utility.asm
@@ -3,4 +3,27 @@ reload_cr3:
     mov rax, cr3
     mov cr3, rax
     ret
+global asm_spinlock_lock
+global asm_spinlock_unlock
+asm_spinlock_lock:
+    mov rax, 0
+    lock bts dword [rdi], 0
+    jc spin ; wait
+    ret
 
+spin: ; never gonna lock you up never gonna lock you doooown
+    inc rax
+    cmp rax, 0xfffffff
+    je something_is_bad_i_want_to_die
+    pause
+    test dword [rdi], 1 ; finnaly the lock is not here
+    jnz spin ; redo again
+    jmp asm_spinlock_lock ; so redo it and lock it
+
+
+
+something_is_bad_i_want_to_die:
+    jmp something_is_bad_i_want_to_die
+asm_spinlock_unlock:
+    lock btr dword [rdi], 0 ; Set the bit to 0
+    ret  ; unlocking :D

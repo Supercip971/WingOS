e14acd1b9c7e5439eabf6906a4edbcd1b29cc58f
Supercip971 <supercyp971@gmail.com>
2020-09-23 16:21:38 -0400

    vmm added the ability to fork vmm dir

--- src/arch/mem/virtual.cpp
+++ src/arch/mem/virtual.cpp
@@ -10,7 +10,58 @@
 #include <utility.h>
 main_page_table *kernel_super_dir;
 main_page_table *main_pml4 = kernel_super_dir;
+uint64_t entry_to_address(uint64_t pml4, uint64_t pdpt, uint64_t pd, uint64_t pt)
+{
+    uint64_t result = 0;
+    result |= pml4 << 39;
+    result |= pdpt << 30;
+    result |= pd << 21;
+    result |= pt << 12;
+    return result;
+}
+int map_page(main_page_table *table, uint64_t phys_addr, uint64_t virt_addr, uint64_t flags)
+{
+    uint64_t pml4_entry = PML4_GET_INDEX(virt_addr);
+    uint64_t pdpt_entry = PDPT_GET_INDEX(virt_addr);
+    uint64_t pd_entry = PAGE_DIR_GET_INDEX(virt_addr);
+    uint64_t pt_entry = PAGE_TABLE_GET_INDEX(virt_addr);
 
+    main_page_table *pdpt, *pd, *pt;
+
+    if (table[pml4_entry] & BIT_PRESENT)
+    {
+        pdpt = (uint64_t *)(get_mem_addr(table[pml4_entry] & FRAME_ADDR));
+    }
+    else
+    {
+        pdpt = (uint64_t *)get_mem_addr((uint64_t)pmm_alloc_zero(1));
+        table[pml4_entry] = (uint64_t)(get_rmem_addr((uint64_t)pdpt)) | PAGE_TABLE_FLAGS;
+    }
+
+    if (pdpt[pdpt_entry] & BIT_PRESENT)
+    {
+        pd = (uint64_t *)(get_mem_addr(pdpt[pdpt_entry] & FRAME_ADDR));
+    }
+    else
+    {
+        pd = (uint64_t *)get_mem_addr((uint64_t)pmm_alloc_zero(1));
+        pdpt[pdpt_entry] = (uint64_t)get_rmem_addr((uint64_t)pd) | PAGE_TABLE_FLAGS;
+    }
+
+    if (pd[pd_entry] & BIT_PRESENT)
+    {
+        pt = (uint64_t *)(get_mem_addr(pd[pd_entry] & FRAME_ADDR));
+    }
+    else
+    {
+        pt = (uint64_t *)get_mem_addr((uint64_t)pmm_alloc_zero(1));
+        pd[pd_entry] = (uint64_t)get_rmem_addr((uint64_t)pt) | PAGE_TABLE_FLAGS;
+    }
+
+    pt[pt_entry] = (uint64_t)(phys_addr | flags);
+
+    return 0;
+}
 int map_page(uint64_t phys_addr, uint64_t virt_addr, uint64_t flags)
 {
     uint64_t pml4_entry = PML4_GET_INDEX(virt_addr);
@@ -54,7 +105,59 @@ int map_page(uint64_t phys_addr, uint64_t virt_addr, uint64_t flags)
 
     return 0;
 }
+main_page_table *fork_vmm_page_dir()
+{
 
+    main_page_table *ret_pml4 = (uint64_t *)get_mem_addr((uint64_t)pmm_alloc_zero(1));
+
+    uint64_t *pdpt;
+    uint64_t *pd;
+    uint64_t *pt;
+    for (int i = 0; i < 512; i++)
+    { // pml4
+        if (main_pml4[i] & 1)
+        {
+            pdpt = (uint64_t *)(get_mem_addr(main_pml4[i] & FRAME_ADDR));
+            for (int j = 0; j < 512; j++)
+            { // pdpt
+                if (pdpt[j] & 1)
+                {
+                    pd = (uint64_t *)(get_mem_addr(pdpt[j] & FRAME_ADDR));
+                    for (int k = 0; k < 512; k++)
+                    { // pd
+                        if (pd[k] & 1)
+                        {
+                            pt = (uint64_t *)(get_mem_addr(pd[k] & FRAME_ADDR));
+                            for (int l = 0; l < 512; l++)
+                            { // pt
+                                /* see that mountain */
+                                if (pt[l] & 1)
+                                {
+                                    /* oh there is a cave ! */ //  map_page(ret_pml4, , entry_to_address(i,j,k,l), 0x03, pt[l] & 0xfff);
+                                    uint64_t end_virtual_addres = entry_to_address(i, j, k, l);
+                                    uint64_t end_physical_addres = pt[l] & ~(0xfff);
+                                    //         void* new_physical = pmm_alloc(1);
+                                    map_page(ret_pml4, end_physical_addres, end_virtual_addres, pt[l] & (0xfff));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return ret_pml4;
+}
+void set_vmm_page_dir(main_page_table *table)
+{
+    main_pml4 = table;
+    update_paging();
+}
+void set_vmm_kernel_page_dir()
+{
+    main_pml4 = kernel_super_dir;
+    update_paging();
+}
 void init_vmm(stivale_struct *bootdata)
 {
     log("vmm", LOG_DEBUG) << "loading vmm";
@@ -98,6 +201,8 @@ void init_vmm(stivale_struct *bootdata)
     }
 
     set_paging_dir(get_rmem_addr((uint64_t)main_pml4));
+   // main_pml4 = fork_vmm_page_dir(); for testing
+   // set_paging_dir(get_rmem_addr((uint64_t)main_pml4));
 
     log("vmm", LOG_INFO) << "loading vmm done";
 }

b97457b06a8c8eb2f6c760e4f64f576894830f66
Supercip971 <supercyp971@gmail.com>
2020-09-27 08:39:27 -0400

    fix: clear lock when time out reached

--- src/arch/lock.cpp
+++ src/arch/lock.cpp
@@ -5,4 +5,5 @@
 extern "C" void something_is_bad_i_want_to_die_higher_level(lock_type *the_bad_guy)
 {
     log("lock", LOG_ERROR) << "lock : " << the_bad_guy->file << " line : " << the_bad_guy->line << "take too long time to unlock";
+    the_bad_guy->data = 0;
 }

8fb0ef7be4f09fca34b0ba952f47b56841f5797f
Supercip971 <supercyp971@gmail.com>
2020-09-28 03:28:12 -0400

    use a lock in print service

--- src/kernel_service/print_service.cpp
+++ src/kernel_service/print_service.cpp
@@ -1,7 +1,9 @@
+#include <arch/lock.h>
 #include <arch/process.h>
 #include <com.h>
 #include <kernel_service/print_service.h>
 
+lock_type locker = {0};
 void print_service()
 {
     log("console_out", LOG_INFO) << "loaded print service";
@@ -14,13 +16,14 @@ void print_service()
         }
         else
         {
-            asm("cli");
+            lock((&locker));
             log("console_out", LOG_INFO); // this is very insecure :(
             com_write_strn((char *)msg->content_address, msg->content_length);
             msg->has_been_readed = true;
             msg->response = 10; // 10 for yes
             on_request_service_update();
-            asm("sti");
+
+            unlock((&locker));
         }
     }
 }

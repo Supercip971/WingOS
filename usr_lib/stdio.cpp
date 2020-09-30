ab43b375ce1643ded608e65889729c406e174849
Supercip971 <supercyp971@gmail.com>
2020-09-30 14:23:49 -0400

    usrlib: rework of user printf + add sprintf

--- usr_lib/stdio.cpp
+++ usr_lib/stdio.cpp
@@ -5,11 +5,23 @@
 #include <klib/kernel_util.h>
 #include <klib/string_util.h>
 char temp_buf[64];
-int printf(const char* format, ...){
-    va_list parameters;
-    va_start(parameters, format);
 
-    int written = 0;
+
+int vsn_printf_out(bool just_print, char* buffer, uint64_t count, const char* data ){
+    if(just_print == true){
+        sys::write_console(data, count);
+        return 1;
+    }else if(buffer != nullptr){
+        strncpy(buffer, data, count);
+        buffer = buffer + count;
+        return 1;
+    }
+    return 0;
+}
+int vsn_printf(bool just_print, char* buffer /* nullptr say no buffer just print it*/, uint64_t count, const char* format, va_list argument){
+
+
+    size_t written = 0;
 
     while (*format != '\0')
     {
@@ -27,7 +39,7 @@ int printf(const char* format, ...){
                 // TODO: Set errno to EOVERFLOW.
                 return -1;
             }
-            if (!sys::write_console(format, amount))
+            if (!vsn_printf_out(just_print,buffer, amount, format))
                 return written;
             format += amount;
             written += amount;
@@ -39,37 +51,38 @@ int printf(const char* format, ...){
         if (*format == 'c')
         {
             format++;
-            char c = (char)va_arg(parameters, int /* char promotes to int */);
+            char c = (char)va_arg(argument, int /* char promotes to int */);
             if (!maxrem)
             {
                 // TODO: Set errno to EOVERFLOW.
                 return -1;
             }
-            if (!sys::write_console(&c, sizeof(c)))
+            if (!vsn_printf_out(just_print,buffer, sizeof (c), &c))
                 return 0;
             written++;
         }
         else if (*format == 's')
         {
             format++;
-            const char *str = va_arg(parameters, const char *);
+            const char *str = va_arg(argument, const char *);
             uint64_t len = strlen(str);
             if (maxrem < len)
             {
                 // TODO: Set errno to EOVERFLOW.
                 return -1;
             }
-            if (!sys::write_console(str, len))
+            if (!vsn_printf_out(just_print,buffer, len, str))
                 return -1;
             written += len;
         }
         else if (*format == 'x')
         {
             format++;
-            uint64_t d = va_arg(parameters, uint64_t);
-            if (d == 0)
+            uint64_t d = va_arg(argument, uint64_t);
+                if (d == 0)
             {
-                sys::write_console("0", 1);
+                char c = '0';
+                vsn_printf_out(just_print,buffer, sizeof (c), &c);
                 written += 1;
             }
             else
@@ -85,10 +98,11 @@ int printf(const char* format, ...){
                     // TODO: Set errno to EOVERFLOW.
                     return -1;
                 }
-                if (!sys::write_console(temp_buf, len))
+                if (!vsn_printf_out(just_print,buffer, len, temp_buf))
                     return -1;
-                written += len;
+              written += len;
             }
+
         }
         else
         {
@@ -99,13 +113,27 @@ int printf(const char* format, ...){
                 // TODO: Set errno to EOVERFLOW.
                 return -1;
             }
-            if (!sys::write_console(format, len))
+            if (!vsn_printf_out(just_print,buffer, len, format))
                 return -1;
             written += len;
             format += len;
         }
     }
-
-    va_end(parameters);
     return written;
+}
+int sprintf(char* buffer, const char* format, ...){
+
+    va_list va;
+    va_start(va, format);
+    uint64_t return_value = vsn_printf(false, buffer, 0, format, va);
+    va_end((va));
+    return return_value;
+
+}
+int printf(const char* format, ...){
+    va_list va;
+    va_start(va, format);
+    uint64_t return_value = vsn_printf(true, nullptr, 0, format, va);
+    va_end((va));
+    return return_value;
 }
--- usr_lib/stdio.h
+++ usr_lib/stdio.h
@@ -2,3 +2,5 @@
 
 
 int printf(const char* format, ...);
+
+int sprintf(char* buffer, const char* format, ...);

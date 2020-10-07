1a3d3b42d1b8b311cf5f74a358b1f7114ee34855
Supercip971 <supercyp971@gmail.com>
2020-10-07 15:00:18 -0400

    usr_lib: basic widget structure

---
+++ usr_lib/kgui/widget.cpp
@@ -0,0 +1,40 @@
+#include <kgui/widget.h>
+#include <klib/mem_util.h>
+namespace gui {
+
+
+
+    void widget_list::init(uint64_t length){
+        list = (gui::widget**)sys::service_malloc(length * sizeof (gui::widget*));
+        for(uint64_t i = 0; i < length; i++){
+            list[i] = nullptr;
+        }
+        list_length = length;
+    }
+
+    void widget_list::add_widget(widget* widget){
+        for(uint64_t i = 0; i < list_length; i++){
+            if(list[i] == nullptr){
+                list[i] = widget;
+                return;
+            }
+        }
+    }
+
+    void widget_list::update_all(){
+
+        for(uint64_t i = 0; i < list_length; i++){
+            if(list[i] != nullptr){
+                list[i]->update_widget();
+            }
+        }
+    }
+    void widget_list::draw_all(sys::graphic_context& context){
+
+        for(uint64_t i = 0; i < list_length; i++){
+            if(list[i] != nullptr){
+                list[i]->draw_widget(context);
+            }
+        }
+    }
+}
---
+++ usr_lib/kgui/widget.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <klib/graphic_system.h>
+
+namespace gui {
+    class widget
+    {
+    protected:
+        uint64_t widget_width;
+        uint64_t widget_height;
+        uint64_t widget_x;
+        uint64_t widget_y;
+        bool widget_should_draw = false;
+    public:
+        widget();
+
+        virtual void update_widget() = 0;
+        virtual void draw_widget(sys::graphic_context& context) = 0;
+        virtual void init_widget() = 0;
+    };
+
+
+    class widget_list{
+
+
+        widget ** list;
+        uint64_t list_length = 0;
+
+    public:
+
+        void init(uint64_t length);
+
+        void add_widget(widget* widget);
+
+        void update_all();
+        void draw_all(sys::graphic_context& context);
+    };
+}

ed41a64fd7a547c40dfcb5d6483055088b70e8dd
Supercip971 <supercyp971@gmail.com>
2020-10-08 04:40:17 -0400

    usr_lib: added widget list to window

--- usr_lib/kgui/window.cpp
+++ usr_lib/kgui/window.cpp
@@ -9,14 +9,22 @@ namespace gui {
 
         graphic_context.clear_buffer(sys::pixel(100,100,100,0));
         graphic_context.swap_buffer();
+        lst = widget_list();
+        lst.init(128); // wax 128 widget may be increase later, but you will be able to put widget list as a widget so meh
     }
 
     uint64_t window::start(){
         while(true){
+            lst.update_all();
             graphic_context.clear_buffer({100,100,100,255});
             graphic_context.draw_rectangle(0,0,width,20, sys::pixel(80,80,80));
             graphic_context.draw_basic_string((width / 2) - (sys::get_basic_font_width_text(window_name) / 2),20 / 2 - (8 / 2),window_name, sys::pixel(255,255,255));
+            lst.draw_all(graphic_context);
             graphic_context.swap_buffer();
         }
+    }
+    void window::add_widget(widget* wdget){
+       lst.add_widget(wdget);
+       wdget->init_widget();
     }
 }
--- usr_lib/kgui/window.h
+++ usr_lib/kgui/window.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <stdint.h>
 #include <klib/graphic_system.h>
+#include <kgui/widget.h>
 namespace gui {
     class window{
         const char* window_name;
         sys::graphic_context graphic_context;
         uint64_t width;
         uint64_t height;
+        widget_list lst;
         uint64_t current_tick;
         // widget* widget_list; for later ;)
     public:
@@ -15,6 +17,7 @@ namespace gui {
         // it will just skip the update
         // todo: add a after_draw_update() and a before_draw_update()
         window(const char* name, uint64_t window_width, uint64_t window_height);
+        void add_widget(widget* wdget);
         uint64_t start();
     };
 }

6006e0bbe06ea0d3c0d03a4b9b26d001a4932694
Supercip971 <supercyp971@gmail.com>
2020-10-10 12:07:57 -0400

    usr_lib: now you can get a pid from a process name

--- usr_lib/klib/kernel_util.cpp
+++ usr_lib/klib/kernel_util.cpp
@@ -1,5 +1,6 @@
 #include <klib/kernel_util.h>
 #include <klib/process_message.h>
+#include <string.h>
 namespace sys {
 
     int write_console(const char* raw_data, int length){
@@ -11,5 +12,10 @@ namespace sys {
         }
         sys::process_message("console_out", (uint64_t)raw_data, length).read();
         return 1;
+    }
+
+    uint64_t get_process_pid(const char* process_name){
+        uint64_t result = sys::process_message("kernel_process_service", (uint64_t)process_name, strlen(process_name)+1 ).read();
+        return result;
     }
 }
--- usr_lib/klib/kernel_util.h
+++ usr_lib/klib/kernel_util.h
@@ -2,4 +2,6 @@
 
 namespace sys {
     int write_console(const char* raw_data, int length);
+
+    uint64_t get_process_pid(const char* process_name);
 }

c85ed45f46ef3e86c574b32a0723862969797a52
Supercip971 <supercyp971@gmail.com>
2020-10-11 03:55:44 -0400

    [gui] window : now you can get relative mouse position

--- usr_lib/kgui/window.cpp
+++ usr_lib/kgui/window.cpp
@@ -1,6 +1,7 @@
 #include <kgui/window.h>
 #include <klib/graphic_system.h>
 #include <klib/mem_util.h>
+#include <klib/mouse.h>
 namespace gui {
     window::window(const char* name, uint64_t window_width, uint64_t window_height) : graphic_context(window_width, window_height, name){
         width = window_width;
@@ -17,7 +18,7 @@ namespace gui {
         while(true){
             lst.update_all();
             graphic_context.clear_buffer({100,100,100,255});
-            graphic_context.draw_rectangle(0,0,width,20, sys::pixel(80,80,80));
+            graphic_context.draw_rectangle(0,0,width,20, sys::pixel(75,75,75));
             graphic_context.draw_basic_string((width / 2) - (sys::get_basic_font_width_text(window_name) / 2),20 / 2 - (8 / 2),window_name, sys::pixel(255,255,255));
             lst.draw_all(graphic_context);
             graphic_context.swap_buffer();
@@ -27,5 +28,15 @@ namespace gui {
        lst.add_widget(wdget);
        wdget->init_widget(this);
     }
+
+    int32_t window::get_mouse_pos_relative_x(){
+        int32_t wx = graphic_context.get_graphic_context_position().rpos.x;
+
+        return sys::get_mouse_x() - wx;
+    }
+    int32_t window::get_mouse_pos_relative_y(){
+        int32_t wy = graphic_context.get_graphic_context_position().rpos.y;
+
+        return sys::get_mouse_y() - wy;
     }
 }
--- usr_lib/kgui/window.h
+++ usr_lib/kgui/window.h
@@ -18,6 +18,11 @@ namespace gui {
         // todo: add a after_draw_update() and a before_draw_update()
         window(const char* name, uint64_t window_width, uint64_t window_height);
         void add_widget(widget* wdget);
+
+        // get mouse position inside the window
+        int32_t get_mouse_pos_relative_x();
+        int32_t get_mouse_pos_relative_y();
+
         uint64_t start();
     };
 }

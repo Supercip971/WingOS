9cb417a8dacac4a0b06d35e246772adba33da380
Supercip971 <supercyp971@gmail.com>
2020-10-15 13:16:28 -0400

    [usr_lib] added new/delete and new[]/delete[]

--- usr_lib/stddef.h
+++ usr_lib/stddef.h
@@ -2,3 +2,8 @@
 
 #define NULL 0
 typedef unsigned long size_t;
+
+void *operator new(size_t size);
+void *operator new[](size_t size);
+void operator delete(void *p);
+void operator delete[](void *p);

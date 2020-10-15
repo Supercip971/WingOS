6b43a8afc2600e4d7e3231077e9afabd4d7ee8d4
Supercip971 <supercyp971@gmail.com>
2020-10-15 11:43:15 -0400

    [kernel] logging : print more info (cpu+pid)

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,30 +1,39 @@
 #include <com.h>
+#include <device/local_data.h>
 #include <logging.h>
 logging::logging()
 {
 }
 void logging ::set_log_type(const char *data, log_state log_state)
 {
-    if (log_state == 0)
+    uint64_t pid = 0;
+    uint64_t current_cpu = 0;
+    if (get_current_cpu()->current_process != nullptr)
     {
-        printf("\n\033[36m [ %s ] :\033[0m ", data);
+        pid = get_current_cpu()->current_process->pid;
+        current_cpu = apic::the()->get_current_processor_id();
     }
-    else if (log_state == 1)
+
+    if (log_state == LOG_INFO)
+    {
+        printf("\n\033[36m[C%x/T%x] [%s] :\033[0m ", current_cpu, pid, data);
+    }
+    else if (log_state == LOG_DEBUG)
     {
 
-        printf("\n\n\033[1m\033[32m [ %s ] :\033[0m ", data);
+        printf("\n\n\033[1m\033[32m[C%x/T%x] [%s] :\033[0m ", current_cpu, pid, data);
     }
-    else if (log_state == 2)
+    else if (log_state == LOG_ERROR)
     {
-        printf("\n\033[31;1m [ %s ] :\033[0m ", data);
+        printf("\n\033[31;1m[C%x/T%x] [%s] :\033[0m ", current_cpu, pid, data);
     }
     else if (log_state == LOG_WARNING)
     {
-        printf("\n\033[31m [ %s ] :", data);
+        printf("\n\033[31m[C%x/T%x] [%s] :", current_cpu, pid, data);
     }
     else
     {
-        printf("\n\033[4m\033[1m\033[31m [ FATAL ] [ %s ] : \033[0m", data);
+        printf("\n\033[4m\033[1m\033[31m[C%x/T%x][%s][ FATAL ]: \033[0m", current_cpu, pid, data);
     }
 }
 logging logging ::operator<<(const char *string)

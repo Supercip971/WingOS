ff0676718796eaa20f3fa6c5a595201136ed3839
Supercip971 <supercyp971@gmail.com>
2020-10-16 09:17:35 -0400

    [kernel] made memory offset function constexpr

--- src/arch/mem/physical.h
+++ src/arch/mem/physical.h
@@ -11,7 +11,7 @@ void *pmm_alloc_fast(uint64_t lenght);
 void *pmm_alloc_zero(uint64_t lenght);
 void pmm_free(void *where, uint64_t lenght);
 // higher half memory offset
-inline uint64_t get_mem_addr(uint64_t addr) { return addr + 0xffff800000000000; }
-inline uint64_t get_rmem_addr(uint64_t addr) { return addr - 0xffff800000000000; }
-inline uint64_t get_kern_addr(uint64_t addr) { return addr + 0xffffffff80000000; }
-inline uint64_t get_rkern_addr(uint64_t addr) { return addr - 0xffffffff80000000; }
+inline constexpr uint64_t get_mem_addr(const uint64_t addr) { return addr + 0xffff800000000000; }
+inline constexpr uint64_t get_rmem_addr(const uint64_t addr) { return addr - 0xffff800000000000; }
+inline constexpr uint64_t get_kern_addr(const uint64_t addr) { return addr + 0xffffffff80000000; }
+inline constexpr uint64_t get_rkern_addr(const uint64_t addr) { return addr - 0xffffffff80000000; }

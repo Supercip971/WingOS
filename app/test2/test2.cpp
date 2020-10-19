6fc174da0f0adcdfb942d63dcb4a0c809df297d6
Supercip971 <supercyp971@gmail.com>
2020-10-19 09:20:53 -0400

    some change in the test2 app

--- app/test2/test2.cpp
+++ app/test2/test2.cpp
@@ -2,14 +2,7 @@
 #include <klib/process_message.h>
 
 int main(){
-    sys::syscall(0, 1,34,3,4,5);
     while (true) {
-        sys::raw_process_message* msg = sys::service_read_current_queue();
-        if(msg != 0x0){
-            msg->has_been_readed = true;
-            msg->response = 11;
-            sys::syscall(0, 0,1,0,0,0);
-        }
 
 
     }

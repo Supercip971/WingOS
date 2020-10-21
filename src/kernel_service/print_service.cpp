79f88eec6d501b8b8169c658549cb49e75970969
Supercip971 <supercyp971@gmail.com>
2020-10-21 07:18:51 -0400

    [kernel] fix print service wasn't working

--- src/kernel_service/print_service.cpp
+++ src/kernel_service/print_service.cpp
@@ -14,14 +14,14 @@ void print_service()
 
         if (msg != 0)
         {
-            lock((&locker));
-            log("console_out", LOG_INFO); // this is very insecure :(
             com_write_strn((char *)msg->content_address, msg->content_length);
             msg->has_been_readed = true;
             msg->response = 10; // 10 for yes
-            on_request_service_update();
+        }
+        else if (msg == 0)
+        {
 
-            unlock((&locker));
+            on_request_service_update();
         }
     }
 }

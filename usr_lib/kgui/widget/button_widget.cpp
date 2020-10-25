00cbbb35762f8d973ed20a4ee4d79cd9a6af773f
Supercip971 <supercyp971@gmail.com>
2020-10-25 08:37:15 -0400

    [gui] now button widget has rounded corner !

--- usr_lib/kgui/widget/button_widget.cpp
+++ usr_lib/kgui/widget/button_widget.cpp
@@ -34,7 +34,7 @@ namespace gui
                     uint32_t y = parent->get_mouse_pos_relative_y();
                     if (y > widget_y)
                     {
-                        if (y < widget_y + widget_height)
+                        if (y <= widget_y + widget_height)
                         {
                             is_hovered = true;
                         }
@@ -61,15 +61,15 @@ namespace gui
     };
     void button_widget::draw_widget(sys::graphic_context &context)
     {
-
         if (is_hovered)
         {
-            context.draw_rectangle(widget_x, widget_y, widget_width, widget_height, {100, 70, 70, 255});
+            context.draw_rounded_rectangle(4, widget_x, widget_y, widget_width, widget_height, {115, 70, 70, 255});
         }
         else
         {
-            context.draw_rectangle(widget_x, widget_y, widget_width, widget_height, {80, 80, 80, 255});
+            context.draw_rounded_rectangle(4, widget_x, widget_y, widget_width, widget_height, {80, 80, 80, 255});
         }
+        //  context.draw_rounded_rectangle(6, widget_x, widget_y, widget_width, widget_height, {200, 200, 200, 255});
         context.draw_basic_string(widget_x + (widget_width / 2) - text_length / 2, widget_y + (widget_height / 2) - 4, button_title, sys::pixel(255, 255, 255));
     };
     void button_widget::init_widget(void *new_parent)

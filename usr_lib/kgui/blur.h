260878301a1cc048d61302dc1407e37a50c3d756
Supercip971 <supercyp971@gmail.com>
2020-10-28 10:37:57 -0400

    [usr_lib] added a stackblur function

---
+++ usr_lib/kgui/blur.h
@@ -0,0 +1,9 @@
+#pragma once
+void stackblur(unsigned char *src,  ///< input image data
+               unsigned int w,      ///< image width
+               unsigned int h,      ///< image height
+               unsigned int radius, ///< blur intensity (should be in 2..254 range)
+               unsigned int minX,
+               unsigned int maxX,
+               unsigned int minY,
+               unsigned int maxY);

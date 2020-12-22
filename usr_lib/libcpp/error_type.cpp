907927b03ced1756051c405eb73163dbf2a35233
Supercip971 <supercyp971@gmail.com>
2020-12-22 14:14:08 -0400

    [usr_lib] added error_type

---
+++ usr_lib/libcpp/error_type.cpp
@@ -0,0 +1,20 @@
+#include "error_type.h"
+
+namespace wpp
+{
+    error_type::error_type()
+    {
+        value = error_class::SUCCESS;
+    }
+    error_type::error_type(int type)
+    {
+        value = (error_class)type;
+    }
+    error_type::error_type(const char *msg, error_class type, int subtype)
+    {
+
+        value = type;
+        this->msg = msg;
+        this->subtype = subtype;
+    }
+} // namespace wpp

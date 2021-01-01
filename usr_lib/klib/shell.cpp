fbcc2a0ba72856ca4b4c271a41cf1d45209f07de
Supercip971 <supercyp971@gmail.com>
2021-01-01 06:55:28 -0400

    [usr_lib] shell: implement send_key_tap & send_command

--- usr_lib/klib/shell.cpp
+++ usr_lib/klib/shell.cpp
@@ -20,16 +20,17 @@ namespace sys
         shell_protocol prot;
         prot.action = SHELL_SEND_KEY;
         prot.key.keycode = key;
-        //process_message_pid((uint64_t)shell_pid, (uint64_t)&prot, sizeof(shell_protocol)).read();
+        sys::process_message msg = sys::process_message((uint64_t)shell_pid, (uint64_t)&prot, sizeof(shell_protocol));
+        msg.read();
     }
     void shell::send_command(const char *command)
     {
         shell_protocol prot;
         prot.action = SHELL_SEND_COMMAND;
-        prot.command.command = new char[strlen(command) + 2];
+        prot.command.command = new char[strlen(command) + 5];
         memcpy(prot.command.command, command, strlen(command) + 2);
-
-        //process_message_pid((uint64_t)shell_pid, (uint64_t)&prot, sizeof(shell_protocol)).read();
+        sys::process_message msg = sys::process_message((uint64_t)shell_pid, (uint64_t)&prot, sizeof(shell_protocol));
+        msg.read();
         delete[] prot.command.command;
     }
 } // namespace sys

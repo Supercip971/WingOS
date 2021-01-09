f4d9f8c86a1c60333f3bd96e08b916c309bdc251
Supercip971 <supercyp971@gmail.com>
2021-01-09 06:06:31 -0400

    [kernel] added a stack_size element to cpu local data

--- kernel/arch/x86_64/device/local_data.h
+++ kernel/arch/x86_64/device/local_data.h
@@ -21,9 +21,11 @@ public:
     gdtr cgdt;
     tss ctss;
     gdt_descriptor gdt_descriptors[64];
+    static const uint32_t stack_size = 8192;
+    uint8_t stack_data[stack_size] __attribute__((aligned(4096)));
+    uint8_t stack_data_interrupt[stack_size] __attribute__((aligned(4096)));
+    uint8_t syscall_stack[stack_size] __attribute__((aligned(4096)));
 
-    uint8_t stack_data[8192] __attribute__((aligned(4096)));
-    uint8_t stack_data_interrupt[8192] __attribute__((aligned(4096)));
     uint64_t lapic_id;
     main_page_table *page_table;
 

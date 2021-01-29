f35a868ab7a3e8215c545e384f9baae4e88b3fee
Supercip971 <supercyp971@gmail.com>
2021-01-29 07:26:20 -0400

    [kernel] implemented ram file
    and stdio stdin stderr stdzero ...

--- kernel/generic/filesystem/userspace_fs.cpp
+++ kernel/generic/filesystem/userspace_fs.cpp
@@ -4,7 +4,7 @@
 #include <liballoc.h>
 #include <logging.h>
 #include <utility.h>
-
+lock_type handle_lock;
 #define SEEK_SET 0
 #define SEEK_CUR 1
 #define SEEK_END 2
@@ -16,38 +16,100 @@ enum file_system_file_state
     FS_STATE_RESERVED = 3, // may be used later
 
 };
-
+ram_file rmf;
 void init_process_userspace_fs(per_process_userspace_fs &target)
 {
-    /*
-    for (int i = 4; i < per_process_userspace_fs::ram_files_count; i++)
+    lock(&handle_lock);
+    for (int i = 0; i < per_process_userspace_fs::ram_files_count; i++)
     {
-        target.ram_files[i] = new ram_file;
+        target.ram_files[i] = &rmf;
     }
 
     target.ram_files[0] = new std_zero_file();
     target.ram_files[1] = new std_stdout_file();
     target.ram_files[2] = new std_stderr_file();
     target.ram_files[3] = new std_stdin_file();
-*/
+    unlock(&handle_lock);
 }
-int ram_file::read(void *buffer, size_t offset, size_t count)
+size_t ram_file::read(void *buffer, size_t offset, size_t count)
 {
     log("ram file", LOG_ERROR) << "can't use " << __PRETTY_FUNCTION__ << " in file " << get_npath() << " in process" << get_current_cpu_process()->process_name;
     return -1;
 }
-int ram_file::write(const void *buffer, size_t offset, size_t count)
+size_t ram_file::write(const void *buffer, size_t offset, size_t count)
 {
     log("ram file", LOG_ERROR) << "can't use " << __PRETTY_FUNCTION__ << " in file " << get_npath() << " in process" << get_current_cpu_process()->process_name;
     return -1;
 }
 
-/*
-ram_file* ram_dir::get(const char *full_path){
-    log("ram dir", LOG_ERROR) << "can't use " << __PRETTY_FUNCTION__ << " in file " << get_base() << " in process" << get_current_cpu_process()->process_name;
-    return nullptr;
+size_t std_zero_file::read(void *dbuffer, size_t offset, size_t count)
+{
+    memset(dbuffer, 0, count);
+    return offset;
 }
-*/
+size_t std_zero_file::write(const void *dbuffer, size_t offset, size_t count)
+{
+    log("stdzero", LOG_WARNING) << "trying to write to" << get_npath();
+    return 0;
+}
+size_t std_stdbuf_file::read(void *dbuffer, size_t offset, size_t count)
+{
+    size_t final_count = count;
+    if (size < offset)
+    {
+        return 0;
+    }
+    else if (size < final_count + offset)
+    {
+        final_count = size - offset;
+    }
+    memcpy(dbuffer, buffer + offset, count);
+    return final_count;
+}
+size_t std_stdbuf_file::write(const void *dbuffer, size_t offset, size_t count)
+{
+    realocate(offset + count);
+    uint8_t *dbuf = (uint8_t *)dbuffer;
+    for (size_t i = offset; i < offset + count; i++)
+    {
+        if (dbuf[i - offset] == '\0')
+        {
+            buffer[i] = ' ';
+        }
+        else
+        {
+            buffer[i] = dbuf[i - offset];
+        }
+    }
+    for (; logging_pos < offset + count; logging_pos++)
+    {
+        if (buffer[logging_pos] == '\n')
+        {
+            log("std", LOG_INFO) << range_str(buffer + logging_pos_start, logging_pos - logging_pos_start);
+            logging_pos_start = logging_pos + 1;
+        }
+    }
+    return count;
+}
+
+void std_stdbuf_file::realocate(size_t new_size)
+{
+    if (buffer == nullptr)
+    {
+        buffer = (char *)malloc(new_size);
+        memset(buffer, 'f', new_size);
+        size = new_size;
+        return;
+    }
+    else
+    {
+        if (new_size > size)
+        {
+            buffer = (char *)realloc(buffer, new_size);
+        }
+        size = new_size;
+    }
+}
 #define MAX_FILE_HANDLE 255
 filesystem_file_t fs_handle_table[MAX_FILE_HANDLE];
 
@@ -64,16 +126,19 @@ filesystem_file_t *get_if_valid_handle(int fd, bool check_free = true)
     if (fd > MAX_FILE_HANDLE)
     {
         log("fs", LOG_WARNING) << "process " << get_current_cpu_process()->process_name << " trying to use an invalid file descriptor";
+
         return nullptr;
     }
     else if (fs_handle_table[fd].state != file_system_file_state::FS_STATE_USED && check_free)
     {
         log("fs", LOG_WARNING) << "process " << get_current_cpu_process()->process_name << " trying to use a file descriptor already free";
+
         return nullptr;
     }
     else if (fs_handle_table[fd].rpid != get_current_cpu_process()->upid && check_free)
     {
         log("fs", LOG_WARNING) << "process " << get_current_cpu_process()->process_name << " trying to use a file descriptor used by another process";
+
         return nullptr;
     }
     return &fs_handle_table[fd];
@@ -126,9 +191,18 @@ size_t fs_read(int fd, void *buffer, size_t count)
         log("fs", LOG_ERROR) << "process " << get_current_cpu_process()->process_name << " can't read file";
         return 0;
     }
-    auto result = main_fs_system::the()->main_fs()->read_file(file->path, file->cur, count, (uint8_t *)buffer);
-    file->cur += result;
-    return result;
+    if (file->ram_file)
+    {
+        auto result = file->file->read(buffer, file->cur, count);
+        file->cur += result;
+        return result;
+    }
+    else
+    {
+        auto result = main_fs_system::the()->main_fs()->read_file(file->path, file->cur, count, (uint8_t *)buffer);
+        file->cur += result;
+        return result;
+    }
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence)
@@ -150,8 +224,14 @@ size_t fs_lseek(int fd, size_t offset, int whence)
     }
     if (whence == SEEK_END)
     {
-        log("fs", LOG_WARNING) << "process " << get_current_cpu_process()->process_name << " can't seek file: SEEK_END not implemented";
-
+        if (file->ram_file)
+        {
+            file->cur = file->file->size;
+        }
+        else
+        {
+            file->cur = main_fs_system::the()->main_fs()->get_file_length(file->path);
+        }
         // unimplemented :^(
     }
     return file->cur;
@@ -165,31 +245,59 @@ size_t fs_write(int fd, const void *buffer, size_t count)
         log("fs", LOG_ERROR) << "process " << get_current_cpu_process()->process_name << " can't read file";
         return 0;
     }
-    return main_fs_system::the()->main_fs()->write_file(file->path, file->cur, count, (uint8_t *)buffer);
+    if (file->ram_file)
+    {
+        auto result = file->file->write(buffer, file->cur, count);
+        file->cur += result;
+        return result;
+    }
+    else
+    {
+
+        auto result = main_fs_system::the()->main_fs()->write_file(file->path, file->cur, count, (uint8_t *)buffer);
+        file->cur += result;
+        return result;
+    }
 }
 
 int fs_open(const char *path_name, int flags, int mode)
 {
+    lock(&handle_lock);
     int fd = get_free_handle();
     if (fd == -1)
     {
         log("fs", LOG_ERROR) << "process " << get_current_cpu_process()->process_name << " can't get free handle";
+
         return 0;
     }
     set_used_handle(fd);
+    unlock(&handle_lock);
     fs_handle_table[fd].rpid = get_current_cpu_process()->upid;
     fs_handle_table[fd].path = path_name;
     fs_handle_table[fd].mode = mode;
     fs_handle_table[fd].cur = 0;
-    if (main_fs_system::the()->main_fs()->get_file_length(path_name) == (uint64_t)-1)
+    fs_handle_table[fd].ram_file = false;
+    if (is_ram_file(path_name))
     {
-        log("fs", LOG_WARNING) << "process " << get_current_cpu_process()->process_name << " trying to open file" << path_name << "but it doesn't exits";
 
-        return -1;
+        //  log("fs", LOG_INFO) << "process " << get_current_cpu_process()->process_name << " open (ram file) " << path_name;
+        fs_handle_table[fd].ram_file = true;
+        fs_handle_table[fd].file = get_ram_file(path_name);
+        return fd;
     }
-    log("fs", LOG_INFO) << "process " << get_current_cpu_process()->process_name << " open " << path_name;
+    else
+    {
 
-    return fd;
+        if (main_fs_system::the()->main_fs()->get_file_length(path_name) == (uint64_t)-1)
+        {
+            log("fs", LOG_WARNING) << "process " << get_current_cpu_process()->process_name << " trying to open file" << path_name << "but it doesn't exits";
+
+            return -1;
+        }
+        log("fs", LOG_INFO) << "process " << get_current_cpu_process()->process_name << " open " << path_name;
+
+        return fd;
+    }
 }
 
 int fs_close(int fd)
@@ -198,10 +306,40 @@ int fs_close(int fd)
     if (file == nullptr)
     {
         log("fs", LOG_ERROR) << "process " << get_current_cpu_process()->process_name << " invalid fd for closing file ";
+
         return 0;
     }
+    lock(&handle_lock);
 
     set_free_handle(fd);
     log("fs", LOG_INFO) << "process " << get_current_cpu_process()->process_name << " closed " << file->path;
+
+    unlock(&handle_lock);
     return 1;
+}
+
+bool is_ram_file(const char *path)
+{
+
+    for (int i = 0; i < per_process_userspace_fs::ram_files_count; i++)
+    {
+        if (strcmp(get_current_cpu_process()->ufs.ram_files[i]->get_npath(), path) == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+ram_file *get_ram_file(const char *path)
+{
+
+    for (int i = 0; i < per_process_userspace_fs::ram_files_count; i++)
+    {
+        if (strcmp(get_current_cpu_process()->ufs.ram_files[i]->get_npath(), path) == 0)
+        {
+            return get_current_cpu_process()->ufs.ram_files[i];
+        }
+    }
+    return nullptr;
 }
--- kernel/generic/filesystem/userspace_fs.h
+++ kernel/generic/filesystem/userspace_fs.h
@@ -3,6 +3,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+class ram_file
+{ // ram_file are file that are stored in the ram such as stdout stdin ...
+public:
+    char *buffer = nullptr;
+    size_t size = 0;
+    virtual const char *get_npath() { return "invalid ram file"; };
+    virtual size_t read(void *dbuffer, size_t offset, size_t count);
+    virtual size_t write(const void *dbuffer, size_t offset, size_t count);
+};
 struct filesystem_file_t
 {
     const char *path;
@@ -10,30 +19,27 @@ struct filesystem_file_t
     size_t cur;
     uint64_t rpid;
     int state;
-};
-class ram_file
-{ // ram_file are file that are stored in the ram such as stdout stdin ...
-public:
-    char *buffer = nullptr;
-    int size = 0;
-    virtual const char *get_npath() { return "invalid ram file"; };
-    virtual int read(void *buffer, size_t offset, size_t count);
-    virtual int write(const void *buffer, size_t offset, size_t count);
+    bool ram_file;
+    class ram_file *file;
 };
 class std_zero_file : public ram_file
 {
 public:
     virtual const char *get_npath() { return "/dev/zero"; };
-    virtual int read(void *buffer, size_t offset, size_t count);
-    virtual int write(const void *buffer, size_t offset, size_t count);
+    virtual size_t read(void *dbuffer, size_t offset, size_t count);
+    virtual size_t write(const void *dbuffer, size_t offset, size_t count);
 };
 
 class std_stdbuf_file : public ram_file
 {
+protected:
+    void realocate(size_t new_size);
+    size_t logging_pos = 0;
+    size_t logging_pos_start = 0;
+
 public:
-    virtual int increase_buffer(int new_size);
-    virtual int read(void *buffer, size_t offset, size_t count);
-    virtual int write(const void *buffer, size_t offset, size_t count);
+    virtual size_t read(void *dbuffer, size_t offset, size_t count);
+    virtual size_t write(const void *dbuffer, size_t offset, size_t count);
 };
 
 class std_stdin_file : public std_stdbuf_file
@@ -58,6 +64,8 @@ struct per_process_userspace_fs
     ram_file *ram_files[ram_files_count]; // in this we have stdio / stdin / stderr ...
 };
 
+bool is_ram_file(const char *path);
+ram_file *get_ram_file(const char *path);
 
 size_t fs_read(int fd, void *buffer, size_t count);
 size_t fs_write(int fd, const void *buffer, size_t count);
@@ -65,4 +73,5 @@ int fs_open(const char *path_name, int flags, int mode);
 int fs_close(int fd);
 size_t fs_lseek(int fd, size_t offset, int whence);
 void init_userspace_fs();
+void init_process_userspace_fs(per_process_userspace_fs &target);
 #endif // USER_FS_H

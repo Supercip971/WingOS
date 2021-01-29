645a95c606457dfd402e75cab4f822892280a0c6
Supercip971 <supercyp971@gmail.com>
2021-01-29 02:40:58 -0400

    [usr_lib] added sleep and usleep

---
+++ usr_lib/unistd.h
@@ -0,0 +1,10 @@
+#ifndef UNISTD_H
+#define UNISTD_H
+#include <stdint.h>
+#include <time.h>
+// FIXME: move it in sys/types.h
+typedef long suseconds_t;
+unsigned int sleep(unsigned int sec);
+
+suseconds_t usleep(suseconds_t sec);
+#endif // UNISTD_H

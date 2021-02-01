a6ec4a813f5c738ba9724b8b4ec7b6b41cae96b8
Supercip971 <supercyp971@gmail.com>
2021-02-01 13:38:26 -0400

    [kernel] added a new logging system (now i must remove the old one)

--- kernel/generic/logging.cpp
+++ kernel/generic/logging.cpp
@@ -2,6 +2,110 @@
 #include <device/local_data.h>
 #include <kernel.h>
 #include <logging.h>
+
+#include <arch.h>
+lock_type log_lock;
+// ------------------------ SHOULD BE REMOVED START ------------------------
+
+// ------------------------ SHOULD BE REMOVED END ------------------------
+
+constexpr const char *log_type_table[5] = {
+    "\n\033[36m[C%x/T%x] [%s] :\033[0m ",
+    "\n\n\033[1m\033[32m[C%x/T%x] [%s] :\033[0m ",
+    "\n\033[31;1m[C%x/T%x] [%s] :\033[0m ",
+    "\n\033[4m\033[1m\033[31m[C%x/T%x][%s][ FATAL ]: \033[0m",
+    "\n\033[31m[C%x/T%x] [%s] :",
+};
+
+void print_log_str(const char *d, log_state state)
+{
+
+    uint64_t pid = 0;
+    uint64_t current_cpu = 0;
+    if (get_current_cpu_process() != nullptr)
+    {
+        pid = get_current_cpu_process()->upid;
+        current_cpu = get_current_cpu_id();
+    }
+    if (state > 4 || state == 3)
+    {
+        printf(log_type_table[LOG_FATAL], current_cpu, pid, d);
+        return;
+    }
+    printf(log_type_table[state], current_cpu, pid, d);
+}
+
+template <>
+void log_print_element<>(void *element)
+{
+    printf("%x", element);
+}
+template <>
+void log_print_element<>(const char *element)
+{
+    printf(element);
+}
+template <>
+void log_print_element<>(char *element)
+{
+    printf(element);
+}
+template <>
+void log_print_element<>(char element)
+{
+    printf("%c", element);
+}
+template <>
+void log_print_element<>(range_str element)
+{
+    find_device<debug_device>()->echo_out(element.rdata, element.rlength);
+}
+template <>
+void log_print_element<>(uint64_t element)
+{
+    printf("%x", element);
+}
+template <>
+void log_print_element<>(uint32_t element)
+{
+    printf("%x", element);
+}
+template <>
+void log_print_element<>(uint16_t element)
+{
+    printf("%x", element);
+}
+template <>
+void log_print_element<>(uint8_t element)
+{
+    printf("%x", element);
+}
+template <>
+void log_print_element<>(int element)
+{
+    printf("%x", element);
+}
+template <>
+void log_print_element<>(long element)
+{
+
+    printf("%x", element);
+}
+
+void slog(const char *msg)
+{
+    debug_device *dev = find_device<debug_device>();
+    dev->echo_out(msg, strlen(msg));
+}
+void start_log()
+{
+    flock(&log_lock);
+}
+void end_log()
+{
+    unlock(&log_lock);
+}
+
 void logging::set_log_type(const char *data, log_state log_state)
 {
     uint64_t pid = 0;
@@ -18,7 +122,6 @@ void logging::set_log_type(const char *data, log_state log_state)
     }
     printf(log_type_table[log_state], current_cpu, pid, data);
 }
-lock_type log_lock;
 logging::logging()
 {
 }
--- kernel/generic/logging.h
+++ kernel/generic/logging.h
@@ -1,17 +1,23 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 #include <com.h>
+#include <general_device.h>
+#include <lock.h>
 #include <print.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <utility.h>
 enum log_state
 {
     LOG_INFO = 0,
     LOG_DEBUG = 1,
     LOG_ERROR = 2,
     LOG_FATAL = 3,
-    LOG_WARNING = 4
+    LOG_WARNING = 4,
+    LOG_NULL = 5,
 };
+
+// ------------------------ SHOULD BE REMOVED START ------------------------
 
 class logging
 {
@@ -31,6 +37,7 @@ public:
     template <typename T>
     logging operator<<(T data);
 };
+// ------------------------ SHOULD BE REMOVED END ------------------------
 
 class range_str
 {
@@ -39,6 +46,114 @@ public:
     const char *rdata;
     size_t rlength;
     range_str(const char *data, size_t size) : rdata(data), rlength(size){};
+};
+template <class T>
+void log_print_element(T element);
+
+template <>
+void log_print_element<>(const char *element);
+
+template <>
+void log_print_element<>(char *element);
+
+template <>
+void log_print_element<>(char element);
+
+template <>
+void log_print_element<>(range_str element);
+
+template <>
+void log_print_element<>(int64_t element);
+
+template <>
+void log_print_element<>(uint64_t element);
+
+template <>
+void log_print_element<>(uint32_t element);
+
+template <>
+void log_print_element<>(int element);
+
+template <>
+void log_print_element<>(uint16_t element);
+
+template <>
+void log_print_element<>(uint8_t element);
+
+template <>
+void log_print_element<>(void *element);
+template <>
+void log_print_element<>(long element);
+
+void print_log_str(const char *d, log_state state);
+
+void slog(const char *msg);
+template <typename arg1>
+constexpr void slog(const char *msg, arg1 &arg)
+{
+    debug_device *dev = find_device<debug_device>();
+    size_t length = 0;
+    for (size_t i = 0; i < strlen(msg); i++)
+    {
+        if (i != 0 && msg[i] == '}' && msg[i - 1] == '{')
+        {
+            length = i + 1;
+            break;
+        }
+    }
+    if (length == 0)
+    {
+        length = strlen(msg);
+        dev->echo_out(msg, length);
+        return;
+    }
+    else
+    {
+        dev->echo_out(msg, length - 2);
+        log_print_element<arg1>(arg);
+        dev->echo_out(msg + length, strlen(msg) - length);
+    }
+}
+template <typename arg1, typename... argument>
+
+constexpr void slog(const char *msg, arg1 &arg, argument &...args)
+{
+    debug_device *dev = find_device<debug_device>();
+    size_t length = 0;
+    for (size_t i = 0; i < strlen(msg); i++)
+    {
+        if (i != 0 && msg[i] == '}' && msg[i - 1] == '{')
+        {
+            length = i + 1;
+            break;
+        }
+    }
+    if (length == 0)
+    {
+        length = strlen(msg);
+        dev->echo_out(msg, length);
+        return;
+    }
+    else
+    {
+        if (length - 2 > 0)
+        {
+            dev->echo_out(msg, length - 2);
+        }
+        log_print_element<arg1>(arg);
+        return slog(msg + length, args...);
+    }
+}
+void start_log();
+void end_log();
+template <typename... argument>
+constexpr void log(const char *d, log_state state, const char *msg, argument... arg)
+{
+    start_log();
+    print_log_str(d, state);
+    slog(msg, arg...);
+
+    end_log();
 };
 
 template <>
@@ -58,4 +173,5 @@ logging logging::operator<<(unsigned char data);
 template <>
 logging logging::operator<<(range_str data);
 logging log(const char *data, log_state color_mode);
+
 #endif

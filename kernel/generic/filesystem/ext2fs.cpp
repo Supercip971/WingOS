622e336fbdb373db6f2b3f7c8e75c358f21d3f5f
Supercip971 <supercyp971@gmail.com>
2021-02-10 10:40:07 -0400

    [kernel] use new logging in ext2fs

--- kernel/generic/filesystem/ext2fs.cpp
+++ kernel/generic/filesystem/ext2fs.cpp
@@ -89,7 +89,7 @@ bool ext2fs::inode_read(void *buffer, uint64_t cursor, uint64_t count, ext2fs_in
         current_block = (cursor + readed) / block_size;
         if (current_block > parent.strct.block_count)
         {
-            log("ext2fs", LOG_ERROR) << "trying to read out of bound of block at" << current_block << ">" << parent.strct.block_count;
+            log("ext2fs", LOG_ERROR, "trying to read out of bound of block at {} > {}", current_block, parent.strct.block_count);
             break;
         }
         uint64_t chunk_size_to_read = count - readed;
@@ -116,7 +116,7 @@ bool ext2fs::inode_write(const void *buffer, uint64_t cursor, uint64_t count, ex
         current_block = (cursor + writed) / block_size;
         if (current_block > parent.strct.block_count)
         {
-            log("ext2fs", LOG_ERROR) << "trying to write out of bound of block at" << current_block << ">" << parent.strct.block_count;
+            log("ext2fs", LOG_ERROR, "trying to write out of bound of block at {} > {}", current_block, parent.strct.block_count);
             break;
         }
         uint64_t chunk_size_to_write = count - writed;
@@ -137,11 +137,11 @@ uint64_t ext2fs::get_inode_block_map(ext2fs_inode inode_struct, uint64_t block_i
 {
     if (inode_struct.strct.flag & 0x80000)
     {
-        log("ext2fs", LOG_ERROR) << "not supported file flag EXTENT FLAG";
+        log("ext2fs", LOG_ERROR, "not supported file flag EXTENT FLAG");
     };
     if (block_id > inode_struct.strct.block_count)
     {
-        log("ext2fs", LOG_WARNING) << "block" << block_id << "out of bound";
+        log("ext2fs", LOG_WARNING, "block: {} out of bound", block_id);
         return 0;
     }
     uint64_t r = 0;
@@ -174,7 +174,7 @@ uint64_t ext2fs::get_inode_block_map(ext2fs_inode inode_struct, uint64_t block_i
     }
     else
     {
-        log("ext2fs", LOG_WARNING) << "no support for triple indirect block" << block_id;
+        log("ext2fs", LOG_WARNING, "no support for triple indirect block: {}", block_id);
         while (true)
         {
         }
@@ -185,13 +185,14 @@ void ext2fs::add_inode_block_map(ext2fs_inode &inode_struct, uint32_t block_addr
 {
     if (inode_struct.strct.flag & 0x80000)
     {
-        log("ext2fs", LOG_ERROR) << "not supported file flag EXTENT FLAG";
+        log("ext2fs", LOG_ERROR, "not supported file flag EXTENT FLAG");
+        ;
     };
     uint64_t entry_per_blkc = block_size / sizeof(uint32_t);
     uint64_t block_id = inode_struct.strct.block_count - 1;
     inode_struct.strct.block_count++;
 
-    log("ext2fs", LOG_INFO) << "adding block" << inode_struct.strct.block_count - 1 << " at " << block_addr;
+    log("ext2fs", LOG_INFO, "adding block {} at {}", inode_struct.strct.block_count - 1, block_addr);
     if (block_id < 12)
     {
         inode_struct.strct.block_ptr[block_id] = block_addr;
@@ -237,7 +238,7 @@ void ext2fs::add_inode_block_map(ext2fs_inode &inode_struct, uint32_t block_addr
     }
     else
     {
-        log("ext2fs", LOG_WARNING) << "no support for writing triple indirect block" << block_id;
+        log("ext2fs", LOG_WARNING, "no support for writing triple indirect block: {}", block_id);
         while (true)
         {
         }
@@ -247,7 +248,7 @@ uint32_t *ext2fs::create_inode_block_map(ext2fs_inode inode_struct)
 {
     if (inode_struct.strct.flag & 0x80000)
     {
-        log("ext2fs", LOG_ERROR) << "not supported file flag EXTENT FLAG";
+        log("ext2fs", LOG_ERROR, "not supported file flag EXTENT FLAG");
     };
     uint64_t entry_per_blkc = block_size / sizeof(uint32_t);
     uint32_t *data = (uint32_t *)malloc(((inode_struct.strct.block_count + 2) * sizeof(uint32_t))); // why does clang say that this is garbage ? O.o
@@ -277,7 +278,7 @@ uint32_t *ext2fs::create_inode_block_map(ext2fs_inode inode_struct)
             {
                 if (block_id + sub_entry > inode_struct.strct.block_count)
                 {
-                    log("ext2fs", LOG_INFO) << "double indirect dend" << block_id;
+                    log("ext2fs", LOG_INFO, "double indirect dend {}", block_id);
                     return data;
                 }
                 get_io_device(0)->read_unaligned(
@@ -289,7 +290,7 @@ uint32_t *ext2fs::create_inode_block_map(ext2fs_inode inode_struct)
         }
         else
         {
-            log("ext2fs", LOG_WARNING) << "no support for triple indirect block" << block_id;
+            log("ext2fs", LOG_WARNING, "no support for triple indirect block {}", block_id);
             while (true)
             {
             }
@@ -356,67 +357,67 @@ void ext2fs::print_ext2_feature()
 
     // OPTIONAL FEATURE
 
-    log("ext2", LOG_INFO) << "ext2fs optional features : " << super_block->optional_features;
+    log("ext2", LOG_INFO, "ext2fs optional features: {}", super_block->optional_features);
 
     if (super_block->optional_features & PREALOC_BLOCK)
     {
-        log("ext2", LOG_INFO) << " - optional feature : PREALOC_BLOCK";
+        log("ext2", LOG_INFO, " - optional feature : PREALOC_BLOCK");
     }
     if (super_block->optional_features & AFS_INODE)
     {
-        log("ext2", LOG_INFO) << " - optional feature : AFS_INODE ";
+        log("ext2", LOG_INFO, " - optional feature : AFS_INODE ");
     }
     if (super_block->optional_features & JOURNAL_SUPPORT)
     {
-        log("ext2", LOG_INFO) << " - optional feature : JOURNAL_SUPPORT ";
+        log("ext2", LOG_INFO, " - optional feature : JOURNAL_SUPPORT ");
     }
     if (super_block->optional_features & EXTENDED_INODE_ATTRIB)
     {
-        log("ext2", LOG_INFO) << " - optional feature : EXTENDED_INODE_ATTRIB ";
+        log("ext2", LOG_INFO, " - optional feature : EXTENDED_INODE_ATTRIB ");
     }
     if (super_block->optional_features & RESIZABLE_FS)
     {
-        log("ext2", LOG_INFO) << " - optional feature : RESIZABLE_FS ";
+        log("ext2", LOG_INFO, " - optional feature : RESIZABLE_FS ");
     }
     if (super_block->optional_features & INDEXED_DIRECTORY)
     {
-        log("ext2", LOG_INFO) << " - optional feature : INDEXED_DIRECTORY ";
+        log("ext2", LOG_INFO, " - optional feature : INDEXED_DIRECTORY ");
     }
 
     // READ ONLY FEATURES
-    log("ext2", LOG_INFO) << "ext2fs read only features : " << super_block->read_only_features;
+    log("ext2", LOG_INFO, "ext2fs read only features: {}", super_block->read_only_features);
 
     if (super_block->read_only_features & SPARSE_DESC_TABLE)
     {
-        log("ext2", LOG_INFO) << " - read only feature : SPARSE_DESC_TABLE ";
+        log("ext2", LOG_INFO, " - read only feature : SPARSE_DESC_TABLE ");
     }
     if (super_block->read_only_features & LONG_MODE_FILE_SIZE)
     {
-        log("ext2", LOG_INFO) << " - read only feature : LONG_MODE_FILE_SIZE ";
+        log("ext2", LOG_INFO, " - read only feature : LONG_MODE_FILE_SIZE ");
     }
     if (super_block->read_only_features & BINARY_TREE_DIRECTORY_CONTENT)
     {
-        log("ext2", LOG_INFO) << " - read only feature : BINARY_TREE_DIRECTORY_CONTENT ";
+        log("ext2", LOG_INFO, " - read only feature : BINARY_TREE_DIRECTORY_CONTENT ");
     }
 
     // NEEDED FEATURES
-    log("ext2", LOG_INFO) << "ext2fs needed features : " << super_block->required_features;
+    log("ext2", LOG_INFO, "ext2fs needed features: {}", super_block->required_features);
 
     if (super_block->required_features & COMPRESSION)
     {
-        log("ext2", LOG_INFO) << " - needed feature : COMPRESSION ";
+        log("ext2", LOG_INFO, " - needed feature : COMPRESSION ");
     }
     if (super_block->required_features & DIRECTORY_TYPE_FIELD)
     {
-        log("ext2", LOG_INFO) << " - needed feature : DIRECTORY_TYPE_FIELD ";
+        log("ext2", LOG_INFO, " - needed feature : DIRECTORY_TYPE_FIELD ");
     }
     if (super_block->required_features & JOURNAL_REPLAY_NEEDED)
     {
-        log("ext2", LOG_INFO) << " - needed feature : JOURNAL_REPLAY_NEEDED ";
+        log("ext2", LOG_INFO, " - needed feature : JOURNAL_REPLAY_NEEDED ");
     }
     if (super_block->required_features & JOURNAL_DEVICE_NEEDED)
     {
-        log("ext2", LOG_INFO) << " - needed feature : JOURNAL_DEVICE_NEEDED ";
+        log("ext2", LOG_INFO, " - needed feature : JOURNAL_DEVICE_NEEDED ");
     }
 }
 void ext2fs::list_sub_directory(ext2fs_inode inode, int offset)
@@ -503,21 +504,17 @@ ext2fs_inode ext2fs::get_file(const char *path)
 uint8_t *ext2fs::ext_read_file(const char *path)
 {
     ext_lock.lock();
-    log("ext2fs", LOG_INFO) << "reading " << path;
-    log("ext2fs", LOG_INFO) << "getting " << path;
+    log("ext2fs", LOG_INFO, "reading {}", path);
     ext2fs_inode f = get_file(path);
     if (!f.is_valid())
     {
-        log("ext2fs", LOG_WARNING) << "can't find file " << path << " for " << __PRETTY_FUNCTION__;
+        log("ext2fs", LOG_WARNING, "can't find file {} for {}", path, __PRETTY_FUNCTION__);
         ext_lock.unlock();
         return 0;
     }
     uint8_t *dat = (uint8_t *)malloc(f.strct.lower_size);
-    log("ext2fs", LOG_INFO) << "disk read " << path;
 
     inode_read(dat, 0, f.strct.lower_size, f);
-    log("ext2fs", LOG_INFO) << "disk readed " << path;
-    log("ext2fs", LOG_INFO) << "readed " << path;
 
     ext_lock.unlock();
     return dat;
@@ -533,7 +530,7 @@ bool ext2fs::exist(const char *path)
 uint64_t ext2fs::get_file_length(const char *path)
 {
     ext_lock.lock();
-    log("ext2fs", LOG_INFO) << "getting file length " << path;
+    log("ext2fs", LOG_INFO, "getting file length {}", path);
     ext2fs_inode f = get_file(path);
     if (!f.is_valid())
     {
@@ -542,13 +539,13 @@ uint64_t ext2fs::get_file_length(const char *path)
     }
     uint64_t size = f.strct.lower_size;
     ext_lock.unlock();
-    log("ext2fs", LOG_INFO) << "getting file length " << path << " = " << size;
+    log("ext2fs", LOG_INFO, "getting file length {} = {}", path, size);
     return size;
 }
 void ext2fs::init(uint64_t start_sector, uint64_t sector_count)
 {
     offset = start_sector;
-    log("ext2", LOG_DEBUG) << "loading ext2fs at" << start_sector;
+    log("ext2", LOG_DEBUG, "loading ext2fs at {}", start_sector);
 
     uint8_t *temp_buffer = (uint8_t *)malloc(1024);
     get_io_device(0)->read_unaligned(temp_buffer, 1024, (start_sector + 1024));
@@ -562,19 +559,19 @@ void ext2fs::init(uint64_t start_sector, uint64_t sector_count)
     {
         block_group_descriptor_table = 1;
     }
-    log("ext2", LOG_INFO) << "ext2fs major version" << super_block->version;
-    log("ext2", LOG_INFO) << "ext2fs minor version" << super_block->minor_version;
-    log("ext2", LOG_INFO) << "ext2fs block size" << block_size;
-    log("ext2", LOG_INFO) << "ext2fs inode per group" << super_block->inode_count_per_group;
-    log("ext2", LOG_INFO) << "ext2fs inode size" << super_block->inode_size;
+    log("ext2", LOG_INFO, "ext2fs major version: {}", super_block->version);
+    log("ext2", LOG_INFO, "ext2fs minor version: {}", super_block->minor_version);
+    log("ext2", LOG_INFO, "ext2fs block size: {}", block_size);
+    log("ext2", LOG_INFO, "ext2fs inode per group: {}", super_block->inode_count_per_group);
+    log("ext2", LOG_INFO, "ext2fs inode size: {}", super_block->inode_size);
     print_ext2_feature();
     root_inode = get_inode(2);
     ext2fs_directory *dir = (ext2fs_directory *)malloc(sizeof(ext2fs_directory));
     inode_read(dir, 0, sizeof(ext2fs_directory), root_inode);
-    log("ext2", LOG_INFO) << "root inode dir" << dir->name;
-    log("ext2", LOG_INFO) << "root inode name length" << dir->directory_name_length;
-    log("ext2", LOG_INFO) << "root inode block subdir" << dir->inode_dir;
-    log("ext2", LOG_INFO) << "root inode size " << root_inode.strct.lower_size;
+    log("ext2", LOG_INFO, "root inode dir: {}", dir->name);
+    log("ext2", LOG_INFO, "root inode name length: {}", dir->directory_name_length);
+    log("ext2", LOG_INFO, "root inode block subdir: {}", dir->inode_dir);
+    log("ext2", LOG_INFO, "root inode size: {}", root_inode.strct.lower_size);
     list_sub_directory(root_inode, 0);
     get_file("init_fs/test_directory/test_another.txt");
 }
@@ -586,7 +583,7 @@ uint64_t ext2fs::read_file(const char *path, uint64_t at, uint64_t size, uint8_t
     auto f = get_file(path);
     if (!f.is_valid())
     {
-        log("ext2fs", LOG_WARNING) << "can't find file " << path << " for " << __PRETTY_FUNCTION__;
+        log("ext2fs", LOG_WARNING, "can't find file {} for {}", path, __PRETTY_FUNCTION__);
 
         ext_lock.unlock();
         return 0;
@@ -594,7 +591,7 @@ uint64_t ext2fs::read_file(const char *path, uint64_t at, uint64_t size, uint8_t
     uint64_t readed_size = size;
     if (f.strct.lower_size < at)
     {
-        log("ext2fs", LOG_WARNING) << "cursor of" << path << " is out of file bound ( " << at << " should be <= " << f.strct.lower_size;
+        log("ext2fs", LOG_WARNING, "cursor of {} is out of file bound: {} <= {}", path, at, f.strct.lower_size);
 
         ext_lock.unlock();
         return 0;
@@ -618,7 +615,7 @@ uint64_t ext2fs::write_file(const char *path, uint64_t at, uint64_t size, const 
     auto f = get_file(path);
     if (!f.is_valid())
     {
-        log("ext2fs", LOG_WARNING) << "can't find file " << path << " for " << __PRETTY_FUNCTION__;
+        log("ext2fs", LOG_WARNING, "can't find file {} for {}", path, __PRETTY_FUNCTION__);
 
         ext_lock.unlock();
         return 0;
@@ -628,7 +625,7 @@ uint64_t ext2fs::write_file(const char *path, uint64_t at, uint64_t size, const 
     if (f.strct.block_count * block_size < (write_size))
     {
         /*   resize_file(f, write_size); */
-        log("ext2fs", LOG_WARNING) << "can't resize file " << path;
+        log("ext2fs", LOG_WARNING, "can't resize file {}", path);
         ext_lock.unlock();
         return 0;
     }
@@ -637,10 +634,8 @@ uint64_t ext2fs::write_file(const char *path, uint64_t at, uint64_t size, const 
         f.strct.lower_size = write_size;
     }
     write_inode(f);
-    log("ext2fs", LOG_INFO) << "disk write " << path;
     inode_write(buffer, at, size, f);
     // uint8_t *buffer_copy = (uint8_t *)malloc(write_size + 12);
-    log("ext2fs", LOG_INFO) << "disk write end " << path;
 
     ext_lock.unlock();
     return size;
@@ -654,11 +649,11 @@ uint64_t ext2fs::alloc_block_for_inode(ext2fs_inode &inode)
         while (group.free_block < 1)
         {
             group = read_group_from_group_id(gid);
-            log("ext2fs", LOG_WARNING) << "group haven't free blocks" << gid;
+            log("ext2fs", LOG_WARNING, "group haven't free blocks: {}", gid);
             gid++;
         }
     }
-    log("ext2fs", LOG_INFO) << "block group size " << super_block->block_count_per_group * block_size;
+    log("ext2fs", LOG_INFO, "block group size: {}", super_block->block_count_per_group * block_size);
 
     uint8_t *bitmap = (uint8_t *)malloc(block_size);
     memzero(bitmap, block_size);
@@ -676,7 +671,7 @@ uint64_t ext2fs::alloc_block_for_inode(ext2fs_inode &inode)
     }
     if (!founded)
     {
-        log("ext2fs", LOG_ERROR) << "group haven't free blocks founded " << gid;
+        log("ext2fs", LOG_ERROR, "group haven't free blocks founded {}", gid);
         return false;
     }
     // set founded block as used
@@ -702,14 +697,14 @@ void ext2fs::clear_block(size_t block_addr)
 void ext2fs::resize_file(ext2fs_inode &inode, uint64_t new_size)
 {
     uint64_t block_diff = (new_size - inode.strct.lower_size) / block_size;
-    log("ext2fs", LOG_INFO) << "resizing inode" << inode.id << "from" << inode.strct.lower_size << "to " << new_size << " new block count " << block_diff;
+    log("ext2fs", LOG_INFO, "resizing inode: {} from: {} to: {} .. new block count: {}", inode.id, inode.strct.lower_size, new_size, block_diff);
     block_diff++;
     for (uint64_t i = 0; i < block_diff; i++)
     {
         auto res = alloc_block_for_inode(inode);
-        log("ext2fs", LOG_INFO) << "alloc block ret " << res;
+        log("ext2fs", LOG_INFO, "alloc block ret {}", res);
         add_inode_block_map(inode, res);
-        log("ext2fs", LOG_INFO) << "alloc block rret " << get_inode_block_map(inode, inode.strct.block_count - 1);
+        log("ext2fs", LOG_INFO, "alloc block rret {}", get_inode_block_map(inode, inode.strct.block_count - 1));
     }
     inode.strct.block_count += block_diff;
     write_inode(inode);

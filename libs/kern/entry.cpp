e2aa57a9459b5368e6662ae123a176f24cc2f233
Supercip971 <supercyp971@gmail.com>
2021-02-12 14:54:15 -0400

    [usr_lib] align rsp when starting main in user app

--- libs/kern/entry.cpp
+++ libs/kern/entry.cpp
@@ -2,8 +2,9 @@
 #include <lcxxabi.h>
 extern "C" int main(int argc, char **argv);
 
-extern "C" void __entry_point(int argc, char **argv)
+extern "C" void __entry_point(int argc, char **argv, char **env)
 {
+    asm volatile("and rsp, -16");
     sys::stdin = sys::file("/dev/stdin");
     sys::stdout = sys::file("/dev/stdout");
     sys::stderr = sys::file("/dev/stderr");

0f73417d19d0416e24624be5f1ded1254a93554e
Supercip971 <supercyp971@gmail.com>
2021-02-14 10:46:40 -0400

    [usr_lib] moved max/min to math utils file

---
+++ libs/utils/math.h
@@ -0,0 +1,33 @@
+#ifndef MATH_H
+#define MATH_H
+namespace utils
+{
+
+    template <class A>
+    constexpr A max(const A a, const A b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+    template <class A>
+    constexpr A min(const A a, const A b)
+    {
+        if (a < b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+} // namespace utils
+
+#endif // MATH_H

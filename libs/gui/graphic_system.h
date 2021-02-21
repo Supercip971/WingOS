7c3b34190784cad06560b90f1194d266275f41bf
Supercip971 <supercyp971@gmail.com>
2021-02-21 04:25:16 -0400

    [gui]  fix color struct

--- libs/gui/graphic_system.h
+++ libs/gui/graphic_system.h
@@ -75,9 +75,9 @@ namespace gui
         {
             struct
             {
-                uint8_t r;
-                uint8_t g;
                 uint8_t b;
+                uint8_t g;
+                uint8_t r;
                 uint8_t a;
             };
             uint32_t pix;

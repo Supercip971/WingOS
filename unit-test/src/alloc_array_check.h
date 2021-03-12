91651827322388f717fca0d736296d3a305ca1cb
DaimyoHub <daimyohubcontact@gmail.com>
2021-03-12 22:05:55 +0100

    [utils] refactored alloc_array and added some basic type traits.

--- unit-test/src/alloc_array_check.h
+++ unit-test/src/alloc_array_check.h
@@ -1,6 +1,7 @@
 #pragma once
 
 int alloc_array_creation_test();
+int alloc_array_creation_fill_test();
 int alloc_array_alloc_test();
 int alloc_array_free_test();
 int alloc_array_foreachentry_test();

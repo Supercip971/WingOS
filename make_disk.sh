11014fb16cac574c516550d3587939ceaca72dc9
supercyp <supercyp@protonmail.com>
2021-03-20 12:11:22 -0400

    [disk] use ./mnt instead of /mnt for disk mount

--- make_disk.sh
+++ make_disk.sh
@@ -3,6 +3,7 @@
 # inspired from https://github.com/ozkl/soso/blob/master/create-image.sh (from soso os) made by ozkl <3
 
 DISK="./build/disk.hdd"
+MNT_PATH="./mnt"
 dd if=/dev/zero of=$DISK bs=64M count=1
 
 fdisk $DISK << EOF
@@ -21,16 +22,17 @@ sudo losetup $LOOP_DISK $DISK
 LOOP_DISK_PARTIITION="`losetup -f`"
 sudo losetup $LOOP_DISK_PARTIITION $DISK -o 1048576
 sudo mke2fs $LOOP_DISK_PARTIITION
-sudo mount $LOOP_DISK_PARTIITION /mnt
+mkdir -p $MNT_PATH
+sudo mount $LOOP_DISK_PARTIITION $MNT_PATH
 
-mkdir /mnt/initfs
-cp -r ./initfs/* /mnt/initfs/
-cp ./limine.cfg /mnt/limine.cfg
-cp ./limine/limine.sys /mnt/limine.sys
-cp ./kernel.elf /mnt/kernel.elf
-ls -la /mnt
-ls -la /mnt/initfs
-umount /mnt
+mkdir $MNT_PATH/initfs
+cp -r ./initfs/* $MNT_PATH/initfs/
+cp ./limine.cfg $MNT_PATH/limine.cfg
+cp ./limine/limine.sys $MNT_PATH/limine.sys
+cp ./kernel.elf $MNT_PATH/kernel.elf
+ls -la $MNT_PATH
+ls -la $MNT_PATH/initfs
+umount $MNT_PATH
 losetup -d $LOOP_DISK_PARTIITION
 losetup -d $LOOP_DISK
 sync 

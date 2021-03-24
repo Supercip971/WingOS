55c8cca59410ce4ab9fe44ca7bd05c64c8bed846
supercyp <supercyp@protonmail.com>
2021-03-24 08:49:06 -0400

    [libc] try to exit if an assert fail

--- libs/libc/assert.h
+++ libs/libc/assert.h
@@ -1,9 +1,12 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 #include <stdio.h>
+#include <stdlib.h>
+
 #define assert(expression)                                          \
     if ((expression) == 0)                                          \
     {                                                               \
         printf("(ASSERT) on line %i file %s ", __LINE__, __FILE__); \
+        exit(-1);                                                   \
     };
 #endif // ASSERT_H

6fac98ebc00317e718e54573166bf793f32c3398
supercyp <supercyp@protonmail.com>
2021-03-24 08:52:46 -0400

    [libutils] added no_null a type that assert if we try to give it the null value

---
+++ libs/utils/no_null.cpp
@@ -0,0 +1,1 @@
+#include "no_null.h"
---
+++ libs/utils/no_null.h
@@ -0,0 +1,38 @@
+#ifndef NO_NULL_H
+#define NO_NULL_H
+
+#include <utils/type/is_same.h>
+#include <stddef.h>
+#include <assert.h>
+namespace utils {
+    template<typename T>
+    class no_null{
+        T _value;
+    public:
+        static_assert (!is_same<nullptr_t, T>::value,"no null must not be null" );
+        template<typename T2>
+        no_null(T2 val){
+            static_assert (!is_same<nullptr_t, T2>::value,"no null constructor must not be null" );
+            assert(reinterpret_cast<uintptr_t>(val) != 0);
+            _value = val;
+        }
+        no_null(T val){
+            assert(reinterpret_cast<uintptr_t>(val) != 0);
+            _value = val;
+        }
+        template<typename T2>
+        no_null& operator=(T2 val){
+            static_assert (!is_same<nullptr_t, T2>::value,"no null constructor must not be null" );
+            assert(reinterpret_cast<uintptr_t>(val) != 0);
+            _value = val;
+        }
+
+        remove_pointer<T> operator->(){
+            return _value;
+        }
+
+        T get(){return _value;};
+    };
+
+}
+#endif // NO_NULL_H

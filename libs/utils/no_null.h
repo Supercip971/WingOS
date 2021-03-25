3ac7942fd8d6a2526162c4b81878ed206e1d73af
supercyp <supercyp@protonmail.com>
2021-03-25 14:10:31 -0400

    [libutils] no_null: clang format fix

--- libs/utils/no_null.h
+++ libs/utils/no_null.h
@@ -1,38 +1,45 @@
 #ifndef NO_NULL_H
 #define NO_NULL_H
 
-#include <utils/type/is_same.h>
-#include <stddef.h>
 #include <assert.h>
-namespace utils {
-    template<typename T>
-    class no_null{
+#include <stddef.h>
+#include <utils/type/is_same.h>
+namespace utils
+{
+    template <typename T>
+    class no_null
+    {
         T _value;
+
     public:
-        static_assert (!is_same<nullptr_t, T>::value,"no null must not be null" );
-        template<typename T2>
-        no_null(T2 val){
-            static_assert (!is_same<nullptr_t, T2>::value,"no null constructor must not be null" );
+        static_assert(!is_same<nullptr_t, T>::value, "no null must not be null");
+        template <typename T2>
+        no_null(T2 val)
+        {
+            static_assert(!is_same<nullptr_t, T2>::value, "no null constructor must not be null");
             assert(reinterpret_cast<uintptr_t>(val) != 0);
             _value = val;
         }
-        no_null(T val){
+        no_null(T val)
+        {
             assert(reinterpret_cast<uintptr_t>(val) != 0);
             _value = val;
         }
-        template<typename T2>
-        no_null& operator=(T2 val){
-            static_assert (!is_same<nullptr_t, T2>::value,"no null constructor must not be null" );
+        template <typename T2>
+        no_null &operator=(T2 val)
+        {
+            static_assert(!is_same<nullptr_t, T2>::value, "no null constructor must not be null");
             assert(reinterpret_cast<uintptr_t>(val) != 0);
             _value = val;
         }
 
-        remove_pointer<T> operator->(){
+        remove_pointer<T> operator->()
+        {
             return _value;
         }
 
-        T get(){return _value;};
+        T get() { return _value; };
     };
 
-}
+} // namespace utils
 #endif // NO_NULL_H

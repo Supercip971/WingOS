0ee5837f7771b2bfef272865d08c719cd54dd9f7
supercyp <supercyp@protonmail.com>
2021-03-31 12:17:12 -0400

    [kernel] rtc: use more const function

--- kernel/arch/x86_64/device/rtc.cpp
+++ kernel/arch/x86_64/device/rtc.cpp
@@ -3,9 +3,6 @@
 #include <kernel.h>
 #include <logging.h>
 RTC main_rtc;
-RTC::RTC()
-{
-}
 
 int rtc_is_updating()
 {
@@ -13,37 +10,37 @@ int rtc_is_updating()
     return inb(0x71) & 0x80;
 }
 
-uint8_t RTC::get_sec()
+uint8_t RTC::get_sec() const
 {
     return read(0);
 }
 
-uint8_t RTC::get_min()
+uint8_t RTC::get_min() const
 {
     return read(0x2);
 }
 
-uint8_t RTC::get_hour()
+uint8_t RTC::get_hour() const
 {
     return read(0x4);
 }
 
-uint8_t RTC::get_day()
+uint8_t RTC::get_day() const
 {
     return read(0x7);
 }
 
-uint8_t RTC::get_month()
+uint8_t RTC::get_month() const
 {
     return read(0x8);
 }
 
-uint8_t RTC::get_year()
+uint8_t RTC::get_year() const
 {
     return read(0x9);
 }
 
-uint8_t RTC::read(int reg)
+uint8_t RTC::read(int reg) const
 {
     while (rtc_is_updating())
         ;
@@ -52,7 +49,7 @@ uint8_t RTC::read(int reg)
     return inb(0x71);
 }
 
-uint64_t RTC::get_total_sec()
+uint64_t RTC::get_total_sec() const
 {
     uint64_t result = 0;
     result += (get_year() - 1) * 365 * 24 * 60 * 60;
@@ -64,20 +61,20 @@ uint64_t RTC::get_total_sec()
     return result;
 }
 
-void RTC::init()
+void RTC::init() const
 {
-    log("rtc", LOG_DEBUG) << "loading rtc...";
+    log("rtc", LOG_DEBUG, "loading rtc...");
 
-    log("rtc", LOG_INFO) << "stime : " << get_total_sec();
-    log("rtc", LOG_INFO) << "year  :" << get_year();
-    log("rtc", LOG_INFO) << "month :" << get_month();
-    log("rtc", LOG_INFO) << "day   :" << get_day();
-    log("rtc", LOG_INFO) << "hour  :" << get_hour();
-    log("rtc", LOG_INFO) << "min   :" << get_min();
-    log("rtc", LOG_INFO) << "sec   :" << get_sec();
+    log("rtc", LOG_INFO, "stime: {}", get_total_sec());
+    log("rtc", LOG_INFO, "year : {}", get_year());
+    log("rtc", LOG_INFO, "month: {}", get_month());
+    log("rtc", LOG_INFO, "day  : {}", get_day());
+    log("rtc", LOG_INFO, "hour : {}", get_hour());
+    log("rtc", LOG_INFO, "min  : {}", get_min());
+    log("rtc", LOG_INFO, "sec  : {}", get_sec());
 }
 
-RTC *RTC::the()
+const RTC *RTC::the()
 {
     return &main_rtc;
 }
--- kernel/arch/x86_64/device/rtc.h
+++ kernel/arch/x86_64/device/rtc.h
@@ -1,24 +1,22 @@
 #ifndef RTC_H
 #define RTC_H
 #include <stdint.h>
-// rtc code is just here temporarly for APIC timer
 class RTC
 {
 public:
-    RTC();
-    void init();
-    uint8_t read(int reg);
-    uint8_t get_sec();
-    uint8_t get_min();
-    uint8_t get_hour();
+    void init() const; // wait a const init function ? is this legal ?
+    uint8_t read(int reg) const;
+    uint8_t get_sec() const;
+    uint8_t get_min() const;
+    uint8_t get_hour() const;
 
-    uint8_t get_day();
+    uint8_t get_day() const;
 
-    uint8_t get_month();
+    uint8_t get_month() const;
 
-    uint8_t get_year();
-    uint64_t get_total_sec();
-    static RTC *the();
+    uint8_t get_year() const;
+    uint64_t get_total_sec() const;
+    static const RTC *the();
 };
 
 #endif // RTC_H

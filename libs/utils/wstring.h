46f377c7213be372780966b649e1c4dc556f86f9
supercyp <supercyp@protonmail.com>
2021-04-04 13:15:08 -0400

    [libutils] string: use memory io instead of buffer

--- libs/utils/wstring.h
+++ libs/utils/wstring.h
@@ -2,7 +2,7 @@
 #define WSTRING_H
 #include <stdio.h>
 #include <string.h>
-#include <utils/buffer.h>
+#include <utils/io/memory_io.h>
 namespace utils
 {
 
@@ -14,7 +14,7 @@ namespace utils
     template <>
     string to_str<const char *>(const char *value);
 
-    class string : public memory_buffer<char>
+    class string : public memory_io
     {
 
     protected:
@@ -22,60 +22,50 @@ namespace utils
         size_t string_length;
 
     public:
-        string() : memory_buffer<char>(16)
+        string() : memory_io(16)
         {
             string_length = 0;
         }
 
-        string(const char *value) : memory_buffer<char>(strlen(value) + 16)
+        string(const char *value) : memory_io(strlen(value) + 16)
         {
             this->seek(0);
-            this->write_value(value, strlen(value) + 1);
+            this->write(value, strlen(value) + 1);
             string_length = strlen(value);
         }
 
-        string(const char value) : memory_buffer<char>(16)
+        string(const char value) : memory_io(16)
         {
             this->seek(0);
-            this->write_value(&value, 1);
+            put_back<const char>(value);
             this->seek(1);
-            char null_str = 0;
-            this->write_value(&null_str, 1);
+            put_back<const char>(0);
             string_length = 1;
         }
 
-        string(const char *value, size_t length) : memory_buffer<char>(length + 16)
+        string(const char *value, size_t length) : memory_io(length + 16)
         {
             this->seek(0);
-            this->write_value(value, length);
+            this->write(value, length);
             string_length = length;
         }
 
-        string(const string &value) : memory_buffer<char>((value.length()) + 16)
+        string(const string &value) : memory_io((value.length()) + 16)
         {
             this->seek(0);
-            this->write_value(value.raw_value(), value.length() + 1);
+            this->write(value.data(), value.length() + 1);
             string_length = value.length();
         }
 
-        string(string &value) : memory_buffer<char>((value.length()) + 16)
+        string(string &value) : memory_io((value.length()) + 16)
         {
             this->seek(0);
-            this->write_value(value.raw_value(), value.length() + 1);
+            this->write(value.data(), value.length() + 1);
             string_length = value.length();
-        }
-
-        string(const buffer *value) : memory_buffer<char>((value->get_size()) + 16)
-        {
-            this->seek(0);
-            copy(this, value);
-
-            string_length = strlen((char *)value->raw());
         }
 
         ~string()
         {
-            destroy();
         }
 
         template <typename V>
@@ -87,12 +77,12 @@ namespace utils
 
         char &operator[](size_t v)
         {
-            return *((this->raw_value()) + v);
+            return *((char *)this->data() + v);
         }
 
         char operator[](size_t v) const
         {
-            return *((this->raw_value()) + v);
+            return *((char *)this->data() + v);
         }
 
         char &get(size_t v)
@@ -101,7 +91,7 @@ namespace utils
             {
                 return null_return;
             }
-            return *((this->raw_value()) + v);
+            return *((char *)this->data() + v);
         }
 
         char get(size_t v) const
@@ -110,25 +100,25 @@ namespace utils
             {
                 return null_return;
             }
-            return *((this->raw_value()) + v);
+            return *((char *)this->data() + v);
         }
 
         const char *c_str() const
         {
-            return raw_value();
+            return (const char *)data();
         }
 
-        void append(const string value)
+        void append(const string &value)
         {
             size_t added_length = value.length();
 
             resize(string_length + added_length + 1);
             seek(string_length);
-            write(value.raw(), value.length() + 1);
+            write(value.data(), value.length() + 1);
 
             string_length += added_length;
         }
-        string &operator+=(const string v)
+        string &operator+=(const string &v)
         {
             append(v);
             return *this;

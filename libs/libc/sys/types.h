63868d6ba5cac13684a0a4e98bf61ef8c27f9890
supercyp <supercyp@protonmail.com>
2021-04-12 16:40:49 -0400

    [libc] sys/types: added more type

--- libs/libc/sys/types.h
+++ libs/libc/sys/types.h
@@ -1,6 +1,24 @@
 #ifndef SYS_TYPES_H
 #define SYS_TYPES_H
-typedef long ssize_t;
-typedef int gid_t;
-typedef long pid_t;
+#include <stddef.h>
+typedef long ssize_t; // signed size_t
+
+typedef long blkcnt_t;    // block count
+typedef size_t blksize_t; // block size
+
+typedef blkcnt_t fsblkcnt_t; // file system block count
+
+typedef size_t clock_t; // clock tick
+typedef long clockid_t; // clock id
+
+typedef unsigned long id_t; // generic id
+
+typedef int gid_t;           // group id
+typedef long pid_t;          // process & process group id
+typedef unsigned long uid_t; // user id
+
+typedef long suseconds_t;      // time in microsecond
+typedef long time_t;           // time in second
+typedef unsigned long timer_t; // timer id
+
 #endif

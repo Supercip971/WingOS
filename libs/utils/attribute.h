d8e51c65b6bfba87f90493f6b0b5b09f0678ef35
supercyp <supercyp@protonmail.com>
2021-04-24 08:43:22 -0400

    [libutils] added a utility attribute file

---
+++ libs/utils/attribute.h
@@ -0,0 +1,9 @@
+
+#ifndef ATTRIBUTE_H
+#define ATTRIBUTE_H
+
+#define FORCE_INLINE __attribute__((always_inline))
+#define NO_RETURN [[noreturn]]
+#define ASM_FUNCTION extern "C"
+
+#endif //ATTRIBUTE_H

d2420682b7a06546557a7673395cbf611dc4048c
supercyp <supercyp@protonmail.com>
2021-04-30 15:57:25 -0400

    [kernel] remove unimplemented include

--- kernel/arch/x86_64/module_calls.h
+++ kernel/arch/x86_64/module_calls.h
@@ -3,7 +3,6 @@
 #include <general_device.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <utils/module_flags.h>
 
 #include <module/module_calls.h>
 void set_modules_calls(module_calls_list *list);

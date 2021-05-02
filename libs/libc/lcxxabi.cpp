f634eabd5d984c86c6f563c94ef3cabbba8ef00a
supercyp <supercyp@protonmail.com>
2021-05-02 07:45:59 -0400

    [libc] added lxxabi guard static lock

--- libs/libc/lcxxabi.cpp
+++ libs/libc/lcxxabi.cpp
@@ -51,4 +51,22 @@ extern "C"
         };
     };
 }
+namespace __cxxabiv1
+{
+    extern "C" int __cxa_guard_acquire(__guard *g)
+    {
+        return !*(char *)(g);
+    }
+
+    extern "C" void __cxa_guard_release(__guard *g)
+    {
+        *(char *)g = 1;
+    }
+
+    extern "C" void __cxa_guard_abort(__guard *)
+    {
+        // not implemented as the __cxa_guard can't throw for the moment
+    }
+} // namespace __cxxabiv1
+
 #endif
--- libs/libc/lcxxabi.h
+++ libs/libc/lcxxabi.h
@@ -1,6 +1,7 @@
 #ifndef LCXXABI_H
 #define LCXXABI_H
 #ifndef UNIT_TEST
+#include <utils/lock.h>
 extern "C"
 {
 #define ATEXIT_MAX_FUNCS 128
@@ -17,5 +18,13 @@ extern "C"
     int __cxa_atexit(void (*destructor)(void *), void *target, void *dso_handle);
     void __cxa_finalize(void *targ);
 }
+namespace __cxxabiv1
+{
+    __extension__ typedef int __guard __attribute__((mode(__DI__)));
+    extern "C" int __cxa_guard_acquire(__guard *);
+    extern "C" void __cxa_guard_release(__guard *);
+    extern "C" void __cxa_guard_abort(__guard *);
+
+} // namespace __cxxabiv1
 #endif
 #endif // LCXXABI_H

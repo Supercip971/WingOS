bcfac7ef856d06f0ee929910918c313b06ff350a
supercyp <supercyp@protonmail.com>
2021-05-06 12:04:23 -0400

    [libutils] string: added to_str from int function

--- libs/utils/wstring.cpp
+++ libs/utils/wstring.cpp
@@ -1,5 +1,6 @@
 #include "wstring.h"
 #include <string.h>
+#include <utils/string_util.h>
 namespace utils
 {
 
@@ -14,5 +15,38 @@ namespace utils
     {
         string v((const char *)value);
         return v;
+    }
+
+    template <typename T>
+    string to_str_from_int(T value)
+    {
+
+        char *var = new char[32];
+
+        utils::int_to_string<T>(var, 'd', value);
+
+        string v = string(var);
+        delete[] var;
+        return v;
+    }
+    template <>
+    string to_str<int>(int value)
+    {
+        return to_str_from_int(value);
+    }
+    template <>
+    string to_str<uint32_t>(uint32_t value)
+    {
+        return to_str_from_int(value);
+    }
+    template <>
+    string to_str<uint64_t>(uint64_t value)
+    {
+        return to_str_from_int(value);
+    }
+    template <>
+    string to_str<long>(long value)
+    {
+        return to_str_from_int(value);
     }
 } // namespace utils

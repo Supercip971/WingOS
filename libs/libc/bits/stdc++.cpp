2092c682b14c9612cda03dd0583f18daf973b3a9
supercyp <supercyp@protonmail.com>
2021-05-23 15:17:05 -0400

    [libc] stdc++ added operator new with alignment note: c++ is sketchy

--- libs/libc/bits/stdc++.cpp
+++ libs/libc/bits/stdc++.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 #include <stddef.h>
 #include <stdlib.h>
+
+// this is sketchy
+void *operator new(size_t size, std::align_val_t align)
+{
+    return malloc(size);
+}
+void *operator new[](size_t size, std::align_val_t align)
+{
+
+    return malloc(size);
+}
 void *__attribute__((weak)) operator new(size_t size)
 {
     return malloc(size);
--- libs/libc/bits/stdc++.h
+++ libs/libc/bits/stdc++.h
@@ -2,10 +2,26 @@
 #ifndef STD_CPP_H
 #define STD_CPP_H
 #include <stddef.h>
+typedef decltype(alignof(int)) align_value;
+namespace std
+{
+
+    enum class align_val_t : align_value
+    {
+    };
+
+}
 void *operator new(size_t size);
 void *operator new[](size_t size);
+void *operator new(size_t size, std::align_val_t align);
+void *operator new[](size_t size, std::align_val_t align);
 void operator delete(void *p);
 void operator delete[](void *p);
 void operator delete(void *p);
 void operator delete[](void *p);
+
+inline void *operator new(size_t, void *p) throw() { return p; }
+inline void *operator new[](size_t, void *p) throw() { return p; }
+inline void operator delete(void *, void *) throw(){};
+inline void operator delete[](void *, void *) throw(){};
 #endif

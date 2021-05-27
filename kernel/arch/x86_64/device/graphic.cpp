39c5df0155c31d3d17d0ff5f23836a2da434b367
supercyp <supercyp@protonmail.com>
2021-05-27 14:28:49 -0400

    [kernel] map framebuffer to shared memory

--- kernel/arch/x86_64/device/graphic.cpp
+++ kernel/arch/x86_64/device/graphic.cpp
@@ -13,6 +13,10 @@ basic_framebuffer_graphic_device::basic_framebuffer_graphic_device(size_t width,
         _height = height;
 
         _addr = reinterpret_cast<void *>(get_mem_addr(physical_addr));
+        for (size_t i = 0; i < (width * height * sizeof(uint32_t)) + 4096; i += 4096)
+        {
+            map_page(physical_addr + i, get_mem_addr(physical_addr + i), true, true);
+        }
     }
     else
     {

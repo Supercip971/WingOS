0423b2dd0c00498a4b1a1af68939c63979a43e0a
supercyp <supercyp@protonmail.com>
2021-05-27 14:30:57 -0400

    [kernel] added cpu_registers structure

--- kernel/arch/x86_64/64bit.h
+++ kernel/arch/x86_64/64bit.h
@@ -35,6 +35,32 @@ struct InterruptStackFrame
     uintptr_t ss;
 } __attribute__((packed));
 
+struct cpu_registers
+{
+    uintptr_t r15;
+    uintptr_t r14;
+    uintptr_t r13;
+    uintptr_t r12;
+    uintptr_t r11;
+    uintptr_t r10;
+    uintptr_t r9;
+    uintptr_t r8;
+    uintptr_t rbp;
+    uintptr_t rdi;
+    uintptr_t rsi;
+    uintptr_t rdx;
+    uintptr_t rcx;
+    uintptr_t rbx;
+    uintptr_t rax;
+
+    // Interrupt stack frame
+    uintptr_t rip;
+    uintptr_t cs;
+    uintptr_t rflags;
+    uintptr_t rsp;
+    uintptr_t ss;
+} __attribute__((packed));
+
 extern stivale2_struct boot_loader_data_copy;
 
 inline void outb(uint16_t port, uint8_t value)

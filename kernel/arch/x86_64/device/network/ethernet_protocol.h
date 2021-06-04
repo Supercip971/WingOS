0c16d25b2a8019ca5b36f03c1b4b160cbb012ee4
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-04 09:20:44 -0400

    [kernel] add arp header

--- kernel/arch/x86_64/device/network/ethernet_protocol.h
+++ kernel/arch/x86_64/device/network/ethernet_protocol.h
@@ -20,6 +20,31 @@ struct ethernet_protocol
     mac_address source;
     uint16_t ethernet_type;
     uint8_t payload[];
+} __attribute__((packed));
+
+enum arp_commands : uint16_t
+{
+    ARP_REQUEST = 1,
+    ARP_REPLY = 2,
+    RARP_REQUEST = 3,
+    RARP_REPLY = 4,
+};
+struct Address_resolution_protocol
+{
+    uint16_t link_layer_type;
+    uint16_t protocol_type;
+    uint8_t link_layer_size;
+    uint8_t protocol_size;
+    uint16_t message;
+    uint8_t data[];
+};
+
+struct arp_ipv4_header
+{
+    mac_address sender;
+    uint32_t sender_ip;
+    mac_address receiver;
+    uint32_t receiver_ip;
 };
 
 #endif // ETHERNET_PROTOCOL_H

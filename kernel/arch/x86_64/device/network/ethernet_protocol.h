e73cff4d1b342b07e069e59ffbb081bca153a35f
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-06 12:33:15 -0400

    [network] add even more internet protocol - ip - udp - icmp

--- kernel/arch/x86_64/device/network/ethernet_protocol.h
+++ kernel/arch/x86_64/device/network/ethernet_protocol.h
@@ -14,6 +14,7 @@ enum ethernet_types : uint16_t
     ET_IP6 = 0x86DD,
 };
 
+typedef uint32_t ip_addr;
 struct ethernet_protocol
 {
     mac_address destination;
@@ -29,7 +30,7 @@ enum arp_commands : uint16_t
     RARP_REQUEST = 3,
     RARP_REPLY = 4,
 };
-struct Address_resolution_protocol
+struct address_resolution_protocol
 {
     uint16_t link_layer_type;
     uint16_t protocol_type;
@@ -37,14 +38,46 @@ struct Address_resolution_protocol
     uint8_t protocol_size;
     uint16_t message;
     uint8_t data[];
-};
+} __attribute__((packed));
+
+struct ip_header
+{
+    uint8_t version : 4;
+    uint8_t internet_header_length : 4;
+    uint8_t type_of_service;
+    uint16_t length;
+    uint16_t id;
+    uint16_t flags : 3;
+    uint16_t fragment_offset : 13;
+    uint8_t time_to_live;
+    uint8_t proto;
+    uint16_t check_sum;
+    ip_addr source_addr;
+    ip_addr target_addr;
+} __attribute__((packed));
 
 struct arp_ipv4_header
 {
     mac_address sender;
-    uint32_t sender_ip;
+    ip_addr sender_ip;
     mac_address receiver;
-    uint32_t receiver_ip;
-};
+    ip_addr receiver_ip;
+} __attribute__((packed));
+
+struct udp_header
+{
+    uint16_t source_port;
+    uint16_t target_port;
+    uint16_t length;
+    uint16_t checksum;
+} __attribute__((packed));
+
+struct icmp_v4
+{
+    uint8_t type;
+    uint8_t code;
+    uint16_t check_sum;
+    uint8_t data[];
+}__attribute__((packed));
 
 #endif // ETHERNET_PROTOCOL_H

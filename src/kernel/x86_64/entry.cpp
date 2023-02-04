22bb0503c24910392204cf1dab1b36abbe8def8a
Supercip971 <supercyp971@gmail.com>
2023-02-04 09:17:34 -0400

    style: refactor arch (x86_64) entry code.

--- src/kernel/x86_64/entry.cpp
+++ src/kernel/x86_64/entry.cpp
@@ -1,18 +1,17 @@
 
-#include <mcx/mcx.hpp>
 #include <kernel/kernel.hpp>
 #include <libcore/fmt/log.hpp>
+#include <mcx/mcx.hpp>
+
 #include <arch/x86_64/gdt.hpp>
 
-void arch_entry(const mcx::MachineContext* context)
+void arch_entry(const mcx::MachineContext *context)
 {
     log::log$("started kernel arch");
 
-    {
-        arch::amd64::load_gdt(arch::amd64::default_gdt());
+    arch::amd64::load_gdt(arch::amd64::default_gdt());
+    log::log$("loaded kernel gdt");
 
-
-    }
 
     kernel_entry(context);
 }

f216a58ae08f99dc05778ca500ee936218b5d5e6
cyp <supercyp971@gmail.com>
2023-05-23 10:22:46 -0400

    style: fixed code style in string.c

--- src/libc/string.c
+++ src/libc/string.c
@@ -1,16 +1,21 @@
 #include <libc/string.h>
+
 void *memcpy(void *dest, const void *src, size_t n)
 {
     char *d = (char*)dest;
     const char *s = (const char*)src;
     while (n--)
+    {
         *d++ = *s++;
+    }
     return dest;
 }
 void *memset(void *s, int c, size_t n)
 {
     char *p = (char*)s;
     while (n--)
+    {
         *p++ = c;
+    }
     return s;
 }

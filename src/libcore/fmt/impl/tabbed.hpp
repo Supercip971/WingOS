a44eaaf7a0dfdac6cfc31f9a2dec8c0aafb9b82f
Supercip971 <supercyp971@gmail.com>
2023-08-04 10:01:10 -0400

    feat: add tabed fmt offset util

---
+++ src/libcore/fmt/impl/tabbed.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <libcore/fmt/impl/integers.hpp>
+
+#include "libcore/fmt/flags.hpp"
+
+namespace fmt
+{
+
+struct Tabbed
+{
+    int tab_count;
+    constexpr Tabbed(int tab_count) : tab_count(tab_count) {}
+};
+
+template <core::Writable Targ>
+constexpr core::Result<int> format_v(Targ &target, Tabbed &&value)
+{
+
+    for (int i = 0; i < value.tab_count; i++)
+    {
+        target.write(core::Str("\t"));
+    }
+    return {};
+}
+
+template <core::Writable Targ>
+constexpr core::Result<int> format_v(Targ &target, Tabbed value)
+{
+
+    for (int i = 0; i < value.tab_count; i++)
+    {
+        target.write(core::Str("\t"));
+    }
+    return {};
+}
+} // namespace fmt

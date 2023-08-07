3294c754bd05a2ef96304eabebf644e81a4fc448
Supercip971 <supercyp971@gmail.com>
2023-08-07 14:34:34 -0400

    feat(libcore): add bitmap formatting function

---
+++ src/libcore/fmt/impl/bitmap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <libcore/ds/bitmap.hpp>
+#include <libcore/fmt/fmt.hpp>
+#include <libcore/fmt/impl/integers.hpp>
+
+#include "libcore/fmt/flags.hpp"
+#include "libcore/type/trait.hpp"
+
+#define flags fmt::FMT_HEX | fmt::FMT_CYAN | fmt::FMT_PAD_8BYTES | fmt::FMT_PAD_ZERO
+template <core::IsConvertibleTo<core::Bitmap> T, core::Writable Targ>
+constexpr core::Result<void> format_v(Targ &target, T &value)
+{
+
+    bool last = value.bit(0);
+    size_t begin = 0;
+    size_t end = 0;
+
+    for (size_t i = 0; i < value.len(); i++)
+    {
+        if (last != value.bit(i))
+        {
+            end = i;
+            fmt::format(target, " - [{};{}] -> {}\n", begin | flags, end | flags, last ? "true" : "false");
+            begin = i;
+            last = value.bit(i);
+        }
+    }
+    fmt::format(target, " - [{};{}] -> {}\n", begin | flags, value.len() | flags, last ? "true" : "false");
+
+    return {};
+} // namespace fmt
+#undef flags

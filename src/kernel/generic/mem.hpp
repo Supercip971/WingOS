1d45c9013f6687198018fcb28636615a986194a5
Supercip971 <supercyp971@gmail.com>
2023-08-10 16:18:41 -0400

    feat(kernel): add virtual/physical range utility functions

--- src/kernel/generic/mem.hpp
+++ src/kernel/generic/mem.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <math/range.hpp>
 #include <stdint.h>
 struct VirtAddr
 {
@@ -15,8 +16,34 @@ struct VirtAddr
     template <typename T>
     constexpr T *as(void) { return (T *)_addr; }
 
-    constexpr operator uintptr_t(void) const { return _addr; }
+    constexpr VirtAddr &operator+=(uintptr_t offset)
+    {
+        _addr += offset;
+        return *this;
+    }
+
+    constexpr VirtAddr &operator-=(uintptr_t offset)
+    {
+        _addr -= offset;
+        return *this;
+    }
+
+    constexpr VirtAddr operator+(uintptr_t offset) const { return VirtAddr(_addr + offset); }
+    constexpr VirtAddr operator-(uintptr_t offset) const { return VirtAddr(_addr - offset); }
+    constexpr size_t operator+(const VirtAddr &other) const { return (_addr + other._addr); }
+    constexpr size_t operator-(const VirtAddr &other) const { return (_addr - other._addr); }
+
+    constexpr bool operator<(const VirtAddr &other) const { return _addr < other._addr; }
+    constexpr bool operator>(const VirtAddr &other) const { return _addr > other._addr; }
+    constexpr bool operator<=(const VirtAddr &other) const { return _addr <= other._addr; }
+    constexpr bool operator>=(const VirtAddr &other) const { return _addr >= other._addr; }
+    constexpr bool operator==(const VirtAddr &other) const { return _addr == other._addr; }
+    constexpr bool operator!=(const VirtAddr &other) const { return _addr != other._addr; }
+
+    constexpr explicit operator uintptr_t(void) const { return _addr; }
 };
+
+using VirtRange = math::Range<VirtAddr>;
 
 struct PhysAddr
 {
@@ -39,7 +66,24 @@ struct PhysAddr
 
     constexpr PhysAddr operator+(uintptr_t offset) const { return PhysAddr(_addr + offset); }
     constexpr PhysAddr operator-(uintptr_t offset) const { return PhysAddr(_addr - offset); }
+
+    constexpr size_t operator+(const PhysAddr &other) const { return (_addr + other._addr); }
+    constexpr size_t operator-(const PhysAddr &other) const { return (_addr - other._addr); }
+
+    constexpr bool operator<(const PhysAddr &other) const { return _addr < other._addr; }
+    constexpr bool operator>(const PhysAddr &other) const { return _addr > other._addr; }
+    constexpr bool operator<=(const PhysAddr &other) const { return _addr <= other._addr; }
+    constexpr bool operator>=(const PhysAddr &other) const { return _addr >= other._addr; }
+    constexpr bool operator==(const PhysAddr &other) const { return _addr == other._addr; }
+    constexpr bool operator!=(const PhysAddr &other) const { return _addr != other._addr; }
 };
+
+using PhysRange = math::Range<PhysAddr>;
+
+/* implemented in the bootloader entry */
+/* TODO: move this to a different header */
+extern "C" uintptr_t kernel_physical_base();
+extern "C" uintptr_t kernel_virtual_base();
 
 #if defined(__x86_64)
 #    define MMAP_IO_BASE (0xffff800000000000)
@@ -57,4 +101,18 @@ constexpr inline PhysAddr toPhys(VirtAddr addr)
 constexpr inline VirtAddr toVirt(PhysAddr addr)
 {
     return VirtAddr(addr._addr + MMAP_IO_BASE);
+}
+constexpr inline VirtRange toVirtRange(PhysRange range)
+{
+    return VirtRange(toVirt(range.start()), toVirt(range.end()));
+}
+
+inline VirtAddr toKernel(PhysAddr addr)
+{
+    return VirtAddr(addr._addr + kernel_virtual_base() - kernel_physical_base());
+}
+
+inline VirtRange toKernelRange(PhysRange range)
+{
+    return VirtRange::from_begin_len(toKernel(range.start()), range.len());
 }

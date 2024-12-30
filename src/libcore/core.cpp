80f1da031f7531d1c8f8066270123b4daeb2e3d6
cyp <supercyp971@gmail.com>
2024-12-31 00:06:52 +0100

    feat(libcore): add new/delete operator support

--- src/libcore/core.cpp
+++ src/libcore/core.cpp
@@ -1,5 +1,21 @@
 
-
-void operator delete(void *)
+#include <stdlib.h>
+void *operator new(size_t size)
 {
+    return malloc(size);
+}
+
+void *operator new[](size_t size)
+{
+    return malloc(size);
+}
+
+void operator delete(void *p) noexcept
+{
+    free(p);
+}
+
+void operator delete[](void *p) noexcept
+{
+    free(p);
 }
---
+++ src/libcore/core.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void *operator new(size_t size);
+void *operator new[](size_t size);
+void operator delete(void *p) noexcept;
+void operator delete[](void *p) noexcept;
+
+inline void *operator new(size_t, void *p) throw() { return p; }
+inline void *operator new[](size_t, void *p) throw() { return p; }
+inline void operator delete(void *, void *) throw() {};
+inline void operator delete[](void *, void *) throw() {};

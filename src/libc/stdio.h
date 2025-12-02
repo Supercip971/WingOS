bddb943de0798ff04ef9a27b75c28408422f3601
cyp <supercyp971@gmail.com>
2025-12-02 17:43:09 +0100

    feat(libc): add fopen, puts, fclose, fseek, ftell

--- src/libc/stdio.h
+++ src/libc/stdio.h
@@ -5,9 +5,13 @@
 extern "C" {
     #endif
 #include <stddef.h>
+#include <stdarg.h>
 
 typedef struct FILE FILE;
 
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
 extern FILE* stdin;
 extern FILE* stdout;
 extern FILE* stderr;
@@ -15,8 +19,27 @@ extern FILE* stderr;
 size_t fwrite(void* __restrict ptr, size_t size, size_t n, FILE* __restrict file );
 size_t fread(void* __restrict ptr, size_t size, size_t n, FILE* __restrict file );
 
+// remove and rename
+int remove(const char* filename);
+int rename(const char* old_filename, const char* new_filename);
+
+void fflush(FILE* stream);
+
+int puts(const char* str);
+
+int sscanf(const char* __restrict str, const char* __restrict format, ...);
+
+int putchar(int c);
 int fprintf(FILE *__restrict stream, const char *__restrict format, ...);
 int printf( const char *__restrict format, ...);
+
+int vfprintf(FILE *__restrict stream, const char *__restrict format, va_list arg);
+
+
+FILE* fopen(const char* filename, const char* mode);
+int fclose(FILE* stream);
+int fseek(FILE* stream, long offset, int origin);
+long ftell(FILE* stream);
 
 
 int snprintf ( char * s, size_t n, const char * format, ... );
--- src/libc/stdio_fs.cpp
+++ src/libc/stdio_fs.cpp
@@ -1,3 +1,4 @@
+#include "protocols/vfs/vfs.hpp"
 #include "stdio.h"
 #include <stdarg.h>
 
@@ -93,3 +94,129 @@ size_t fread(void* __restrict ptr, size_t size, size_t n, FILE* __restrict file 
     }
 }
 
+FILE* fopen(const char* filename, const char* mode)
+{
+    (void)mode;
+    prot::VfsConnection vfs = prot::VfsConnection::connect().unwrap();
+    prot::FsFile file = vfs.open_path(core::Str(filename)).unwrap();
+    FILE* f = new FILE();
+    f->kind = FILE_KIND_FILE;
+    f->file = core::move(file);
+    
+    f->cursor = 0;
+    return f;
+
+}
+int remove(const char* filename)
+{
+    log::warn$("remove not implemented yet: {}", filename);
+    // not implemented
+    return 0;
+}
+int rename(const char* old_filename, const char* new_filename)
+{
+    log::warn$("rename not implemented yet: {} {}", old_filename, new_filename);
+    return 0;
+}
+
+int puts(const char* str)
+{
+    size_t len = strlen(str);
+    fwrite((void*)str, 1, len, stdout);
+    char newline = '\n';
+    fwrite((void*)&newline, 1, 1, stdout);
+    return 0;
+}
+
+void fflush(FILE* stream)
+{
+    // no buffering, so nothing to do
+    (void)stream;
+}
+
+int fclose(FILE* stream)
+{
+    switch(stream->kind)
+    {
+        case FILE_KIND_FILE: 
+        {
+            stream->file.close();
+            delete stream;
+            return 0;
+        }
+        case FILE_KIND_OUT: 
+        {
+            delete stream;
+            return 0;
+        }
+        case FILE_KIND_IN:
+        case FILE_KIND_VOID:
+        default: 
+        {
+            return -1;  
+        }
+    }
+}
+int fseek(FILE* stream, long offset, int origin)
+{
+    switch(stream->kind)
+    {
+        case FILE_KIND_FILE: 
+        {
+            switch(origin)
+            {
+                case SEEK_SET:
+                {
+                    stream->cursor = offset;
+                    return 0;
+                }
+                case SEEK_CUR:
+                {
+                    stream->cursor += offset;
+                    return 0;
+                }
+                case SEEK_END:
+                {
+                    auto info = stream->file.get_info();
+                    if (info.is_error())
+                    {
+                        return -1;
+                    }
+                    stream->cursor = info.unwrap().size + offset;
+                    return 0;
+                }
+                default:
+                {
+                    return -1;  
+                }
+            }
+        }
+        case FILE_KIND_OUT: 
+        
+        case FILE_KIND_IN:
+        case FILE_KIND_VOID:
+        default: 
+        {
+            return -1;
+        }
+    }
+
+}
+long ftell(FILE* stream)
+{
+    switch(stream->kind)
+    {
+        case FILE_KIND_FILE: 
+        {
+            return stream->cursor;
+        }
+        case FILE_KIND_OUT: 
+        
+        case FILE_KIND_IN:
+        case FILE_KIND_VOID:
+        default: 
+        {
+            return -1;
+        }
+    }
+}

86becb2b82b2dcb3739dad1dc53aca1ba2146dd3
cyp <supercyp971@gmail.com>
2026-04-29 22:22:11 +0200

    feat: add __cxa guards functions

--- src/libc/stdlib.cpp
+++ src/libc/stdlib.cpp
@@ -5,6 +5,33 @@
 #include "libcore/fmt/flags.hpp"
 #include "libcore/fmt/log.hpp"
 
+namespace __cxxabiv1
+{
+	/* guard variables */
+
+	/* The ABI requires a 64-bit type.  */
+	__extension__ typedef int __guard __attribute__((mode(__DI__)));
+
+	extern "C" int __cxa_guard_acquire (__guard *);
+	extern "C" void __cxa_guard_release (__guard *);
+	extern "C" void __cxa_guard_abort (__guard *);
+
+	extern "C" int __cxa_guard_acquire (__guard *g)
+	{
+		return !*(char *)(g);
+	}
+
+	extern "C" void __cxa_guard_release (__guard *g)
+	{
+		*(char *)g = 1;
+	}
+
+	extern "C" void __cxa_guard_abort (__guard *)
+	{
+
+	}
+}
+
 namespace core
 {
     void dump_vec(uintptr_t _this, uintptr_t _data, bool start)

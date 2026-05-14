448cad37afdbc71ed512c6e0e670d709d1d2d347
cyp <supercyp971@gmail.com>
2026-05-14 10:55:20 +0200

    meta: update encourage.hpp with new messages

--- src/libcore/encourage.hpp
+++ src/libcore/encourage.hpp
@@ -10,11 +10,18 @@ constexpr Array isnt_encouraging_messages =
         "Guru Meditation",
         "Yes but does it have SMP ?",
         "It just works :tm:",
+        "You should check any of the DEVSE member's project, they are all very good and will make you feel better",
+        "Go out, touch some grass, come back to it later...",
         "Errors are social constructions- d0p1",
         "This is a good way to shutdown the computer technically",
         "This would not have happened if you had a blahaj",
-        "Everything is hidden behind an easy fix: https://bit.ly/3dV6cFr"
-
+        "Everything is hidden behind an easy fix: https://bit.ly/3dV6cFr",
+        "Maybe your innie has already fixed this bug",
+        "PROGRAMMING IS DEAD, RAM IS FUEL, DISK IS FULL",
+        "It doesn't happend every day, kinda counted on being a friend, Can I give it up or give it away ?",
+        "- trust us -",
+        "when you entered inside this [COMPUTER], you discover that the inside is bigger than the outside",
+        "Don't shut down the watercooling, or moon will fall",
 };
 
 } // namespace core

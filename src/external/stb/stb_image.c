bd174da4f45875c4319a727840af0be970735ad3
cyp <supercyp971@gmail.com>
2026-05-30 10:52:00 +0200

    deps: add stb_image

---
+++ src/external/stb/stb_image.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifndef STBT_ASSERT
+#    define STBT_ASSERT(x) ((void)(x))
+#endif
+
+#ifndef STBI_ASSERT
+#    define STBI_ASSERT(x) ((void)(x))
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.hpp"
--- src/external/stb/stb_truetype.c
+++ src/external/stb/stb_truetype.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+#ifndef STBTT_assert
+#    define STBTT_assert(x) ((void)(x))
+#endif
+
 #define STB_TRUETYPE_IMPLEMENTATION
-#include "stb_truetype.h"
+#include "stb_truetype.hpp"

c6c797fd88bc04fbf827c9dbbd900083ca368ed5
cyp <supercyp971@gmail.com>
2026-08-22 17:14:25 +0200

    fix(build): correctly use crosscompiler library and export them to sysroot

--- meta/build/make_sysroot.sh
+++ meta/build/make_sysroot.sh
@@ -1,4 +1,9 @@
-mkdir -p meta/build/sysroot/lib 
+mkdir -p meta/build/sysroot/lib
 
 cp -r src/* meta/build/sysroot/lib
 cp -r ./src/libc/** meta/build/sysroot/lib
+
+# technically should always be here
+if [ -d meta/build/cross/x86_64-pc-wingos/lib ]; then
+    cp -r meta/build/cross/x86_64-pc-wingos/lib/* meta/build/sysroot/lib/
+fi
